983999aa06a2ac7e9c8c19f19eb5a92afc1e8f7f
jinyus <starboymc4@gmail.com>
2023-09-22 19:43:50 -0500

    add sript to check output

---
+++ verify.py
@@ -0,0 +1,63 @@
+import json
+import sys
+from typing import Dict, List
+
+
+def generate_correct_related_posts():
+    with open("./related_posts_python.json") as f:
+        python_related: List[Dict] = json.load(f)
+
+    correct_payload: Dict[str, int] = {}
+
+    for i in range(len(python_related)):
+        correct_post = python_related[i]
+        # target_post = target_related[i]
+
+        tag_set = set(correct_post["tags"])
+        shared_tag_count = 0
+
+        for related_post in correct_post["related"]:
+            shared_tag_count += len(set(related_post["tags"]) & tag_set)
+
+        correct_payload[correct_post["_id"]] = shared_tag_count
+
+    with open("./correct_related.json", "w") as f:
+        json.dump(correct_payload, f)
+
+
+if __name__ == "__main__":
+    if len(sys.argv) != 2:
+        print("Usage: python verify.py <path_to_posts.json>")
+        sys.exit(1)
+
+    filepath = sys.argv[1]
+
+    if not filepath.endswith(".json"):
+        print("Generating correct file...")
+        generate_correct_related_posts()
+        sys.exit(0)
+
+    with open(filepath) as f:
+        target_file: List[Dict] = json.load(f)
+
+    with open("correct_related.json") as f:
+        correct_file: Dict[str, int] = json.load(f)
+
+    for post in target_file:
+        tag_set = set(post["tags"])
+        shared_tag_count = 0
+
+        for related_post in post["related"]:
+            shared_tag_count += len(set(related_post["tags"]) & tag_set)
+
+        correct_count = correct_file[post["_id"]]
+
+        if shared_tag_count != correct_count:
+            post_id = post["_id"]
+            print(
+                f"Post {post_id} is invalid!\nexpected: {correct_count} shared tag count\nactual: {shared_tag_count}"
+            )
+            sys.exit(1)
+
+    print("All posts are valid!")
+    sys.exit(0)
