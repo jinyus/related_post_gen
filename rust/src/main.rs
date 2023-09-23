8405abd1b7a29d59ac5b15e12ea8baf5414b6637
Praveen Perera <praveen@avencera.com>
2023-09-22 20:00:05 -0400

    Use rayon with `RB5009` solution

--- rust/src/main.rs
+++ rust/src/main.rs
@@ -1,16 +1,11 @@
-use std::sync::{Arc, Mutex};
-
-use serde::{Deserialize, Serialize};
-use serde_json::from_str;
-// use std::collections::HashMap;
-use rustc_data_structures::fx::FxHashMap;
+use std::collections::BinaryHeap;
+use std::sync::Mutex;
+use std::{cmp::Reverse, sync::Arc};
 
 use rayon::prelude::*;
-
-use mimalloc::MiMalloc;
-
-#[global_allocator]
-static GLOBAL: MiMalloc = MiMalloc;
+use rustc_data_structures::stable_map::FxHashMap;
+use serde::{Deserialize, Serialize};
+use serde_json::from_str;
 
 #[derive(Debug, Serialize, Deserialize, Clone, Eq, PartialEq, Hash)]
 struct Post {
@@ -22,8 +17,8 @@ struct Post {
 
 #[derive(Debug, Serialize)]
 struct RelatedPosts<'a> {
-    _id: &'a String,
-    tags: &'a Vec<String>,
+    _id: &'a str,
+    tags: &'a [String],
     related: Vec<&'a Post>,
 }
 
@@ -31,53 +26,58 @@ fn main() {
     let json_str = std::fs::read_to_string("../posts.json").unwrap();
     let posts: Vec<Post> = from_str(&json_str).unwrap();
 
-    let mut post_tags_map: FxHashMap<&String, Vec<&Post>> = FxHashMap::default();
+    let mut posts_by_tag = FxHashMap::default();
+    posts
+        .iter()
+        .enumerate()
+        .flat_map(|(idx, post)| std::iter::repeat(idx).zip(post.tags.iter().map(|x| x.as_str())))
+        .for_each(|(idx, tag)| {
+            posts_by_tag.entry(tag).or_insert(vec![]).push(idx);
+        });
 
-    for post in &posts {
-        for tag in &post.tags {
-            post_tags_map.entry(tag).or_default().push(post);
-        }
-    }
+    let related_posts = Arc::new(Mutex::new(Vec::with_capacity(posts.len())));
 
-    let related_posts: Arc<Mutex<Vec<RelatedPosts>>> =
-        Arc::new(Mutex::new(Vec::with_capacity(posts.len())));
+    posts.par_iter().enumerate().for_each(|(idx, post)| {
+        let mut tagged_post_count = vec![0; posts.len()];
+        tagged_post_count.fill(0);
 
-    posts.par_iter().for_each(|post| {
-        let mut related_posts_map: FxHashMap<&Post, i8> = FxHashMap::default();
-        related_posts_map.reserve(posts.len());
+        post.tags
+            .iter()
+            .map(|tag| posts_by_tag.get(tag.as_str()))
+            .flatten()
+            .flat_map(|x| x.iter())
+            .filter(|&&tagged_post| tagged_post != idx)
+            .for_each(|&tagged_post| {
+                tagged_post_count[tagged_post] += 1;
+            });
 
-        for tag in &post.tags {
-            if let Some(tag_posts) = post_tags_map.get(tag) {
-                for other_post in tag_posts {
-                    if post._id != other_post._id {
-                        *related_posts_map.entry(other_post).or_default() += 1;
+        let mut top_five = BinaryHeap::new();
+        tagged_post_count
+            .iter()
+            .copied()
+            .enumerate()
+            .filter(|&(_, count)| count > 0)
+            .for_each(|(post, count)| {
+                if top_five.len() < 5 {
+                    top_five.push((Reverse(count), post));
+                } else {
+                    let (Reverse(cnt), _) = top_five.peek().unwrap();
+                    if count > *cnt {
+                        top_five.pop();
+                        top_five.push((Reverse(count), post));
                     }
                 }
-            }
-        }
+            });
 
-        let mut related_posts_for_post: Vec<_> = related_posts_map.into_iter().collect();
-
-        // related_videos_for_video.sort_unstable_by_key(|&(_, count)| -count);
-        related_posts_for_post.sort_by(|post_a, post_b| post_b.1.cmp(&post_a.1));
-
-        related_posts_for_post.truncate(5);
-
-        let mut related = related_posts.lock().unwrap();
-
-        related.push(RelatedPosts {
+        related_posts.lock().unwrap().push(RelatedPosts {
             _id: &post._id,
             tags: &post.tags,
-            related: related_posts_for_post
-                .into_iter()
-                .map(|(post, _)| post)
-                .collect(),
+            related: top_five.into_iter().map(|(_, post)| &posts[post]).collect(),
         });
     });
 
     // Write the result to a JSON file.
-    let related = related_posts.lock().unwrap();
-    let rel = related.as_slice();
-    let json_str = serde_json::to_string(rel).unwrap();
+    let rel = related_posts.lock().unwrap();
+    let json_str = serde_json::to_string(rel.as_slice()).unwrap();
     std::fs::write("../related_posts_rust.json", json_str).unwrap();
 }
