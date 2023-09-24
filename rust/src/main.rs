e63ff86c416f14895640ccf917a5bf72fd36da5e
Scott Lamb <slamb@slamb.org>
2023-09-23 21:33:01 -0700

    ease profiling Rust binaries
    * extract processing to `process` function.
      Using `collect()` is nicer anyway, particularly for the Rayon
      version where `ParallelIterator::collect` avoids locking.
    
    * if given a number of seconds, re-run the algorithm for that time.
    
    * build with enough debugging info to make `samply` happy

--- rust/src/main.rs
+++ rust/src/main.rs
@@ -1,4 +1,8 @@
-use std::{cmp::Reverse, collections::BinaryHeap, time::Instant};
+use std::{
+    cmp::Reverse,
+    collections::BinaryHeap,
+    time::{Duration, Instant},
+};
 
 use rustc_data_structures::fx::FxHashMap;
 use serde::{Deserialize, Serialize};
@@ -23,12 +27,7 @@ struct RelatedPosts<'a> {
     related: Vec<&'a Post>,
 }
 
-fn main() {
-    let json_str = std::fs::read_to_string("../posts.json").unwrap();
-    let posts: Vec<Post> = from_str(&json_str).unwrap();
-
-    let start = Instant::now();
-
+fn process(posts: &[Post]) -> Vec<RelatedPosts<'_>> {
     let mut post_tags_map: FxHashMap<&String, Vec<usize>> = FxHashMap::default();
 
     for (i, post) in posts.iter().enumerate() {
@@ -37,45 +36,75 @@ fn main() {
         }
     }
 
-    let mut related_posts: Vec<RelatedPosts> = Vec::with_capacity(posts.len());
+    posts
+        .iter()
+        .enumerate()
+        .map(|(idx, post)| {
+            // faster than allocating outside the loop
+            let mut tagged_post_count = vec![0; posts.len()];
 
-    for (idx, post) in posts.iter().enumerate() {
-        // faster than allocating outside the loop
-        let mut tagged_post_count = vec![0; posts.len()];
-
-        for tag in &post.tags {
-            if let Some(tag_posts) = post_tags_map.get(tag) {
-                for &other_post_idx in tag_posts {
-                    if idx != other_post_idx {
-                        tagged_post_count[other_post_idx] += 1;
+            for tag in &post.tags {
+                if let Some(tag_posts) = post_tags_map.get(tag) {
+                    for &other_post_idx in tag_posts {
+                        if idx != other_post_idx {
+                            tagged_post_count[other_post_idx] += 1;
+                        }
                     }
                 }
             }
-        }
 
-        let mut top_five = BinaryHeap::new();
-        tagged_post_count
-            .into_iter()
-            .enumerate()
-            .for_each(|(post, count)| {
-                if top_five.len() < 5 {
-                    top_five.push((Reverse(count), post));
-                } else {
-                    let (Reverse(cnt), _) = top_five.peek().unwrap();
-                    if count > *cnt {
-                        top_five.pop();
+            let mut top_five = BinaryHeap::new();
+            tagged_post_count
+                .into_iter()
+                .enumerate()
+                .for_each(|(post, count)| {
+                    if top_five.len() < 5 {
                         top_five.push((Reverse(count), post));
+                    } else {
+                        let (Reverse(cnt), _) = top_five.peek().unwrap();
+                        if count > *cnt {
+                            top_five.pop();
+                            top_five.push((Reverse(count), post));
+                        }
                     }
-                }
-            });
+                });
 
-        related_posts.push(RelatedPosts {
-            _id: &post._id,
-            tags: &post.tags,
-            related: top_five.into_iter().map(|(_, post)| &posts[post]).collect(),
-        });
+            RelatedPosts {
+                _id: &post._id,
+                tags: &post.tags,
+                related: top_five.into_iter().map(|(_, post)| &posts[post]).collect(),
+            }
+        })
+        .collect()
+}
+
+fn main() {
+    let json_str = std::fs::read_to_string("../posts.json").unwrap();
+    let posts: Vec<Post> = from_str(&json_str).unwrap();
+
+    let start = Instant::now();
+    let args: Vec<_> = std::env::args().collect();
+    match &args[..] {
+        [_progname] => {}
+        [_progname, secs] => {
+            let target = start + Duration::from_secs(secs.parse().unwrap());
+            let mut i = 0;
+            let mut now;
+            loop {
+                process(&posts);
+                i += 1;
+                now = Instant::now();
+                if now > target {
+                    break;
+                }
+            }
+            println!("{:?} per iteration", (now - start) / i);
+            return;
+        }
+        _ => panic!("invalid arguments"),
     }
 
+    let related_posts = process(&posts);
     let end = Instant::now();
 
     print!(
--- rust_rayon/src/main.rs
+++ rust_rayon/src/main.rs
@@ -1,8 +1,7 @@
 use std::{
     cmp::Reverse,
     collections::BinaryHeap,
-    sync::{Arc, Mutex},
-    time::Instant,
+    time::{Duration, Instant},
 };
 
 use rayon::prelude::*;
@@ -29,12 +28,7 @@ struct RelatedPosts<'a> {
     related: Vec<&'a Post>,
 }
 
-fn main() {
-    let json_str = std::fs::read_to_string("../posts.json").unwrap();
-    let posts: Vec<Post> = from_str(&json_str).unwrap();
-
-    let start = Instant::now();
-
+fn process(posts: &[Post]) -> Vec<RelatedPosts<'_>> {
     let mut post_tags_map: FxHashMap<&String, Vec<usize>> = FxHashMap::default();
 
     for (i, post) in posts.iter().enumerate() {
@@ -43,47 +37,77 @@ fn main() {
         }
     }
 
-    let related_posts: Arc<Mutex<Vec<RelatedPosts>>> =
-        Arc::new(Mutex::new(Vec::with_capacity(posts.len())));
+    posts
+        .par_iter()
+        .enumerate()
+        .map(|(idx, post)| {
+            let mut tagged_post_count = vec![0; posts.len()];
+            tagged_post_count.fill(0);
 
-    posts.par_iter().enumerate().for_each(|(idx, post)| {
-        let mut tagged_post_count = vec![0; posts.len()];
-
-        for tag in &post.tags {
-            if let Some(tag_posts) = post_tags_map.get(tag) {
-                for &other_post_idx in tag_posts {
-                    if idx != other_post_idx {
-                        tagged_post_count[other_post_idx] += 1;
+            for tag in &post.tags {
+                if let Some(tag_posts) = post_tags_map.get(tag) {
+                    for &other_post_idx in tag_posts {
+                        if idx != other_post_idx {
+                            tagged_post_count[other_post_idx] += 1;
+                        }
                     }
                 }
             }
-        }
 
-        let mut top_five = BinaryHeap::new();
-        tagged_post_count
-            .into_iter()
-            .enumerate()
-            .for_each(|(post, count)| {
-                if top_five.len() < 5 {
-                    top_five.push((Reverse(count), post));
-                } else {
-                    let (Reverse(cnt), _) = top_five.peek().unwrap();
-                    if count > *cnt {
-                        top_five.pop();
+            let mut top_five = BinaryHeap::new();
+            tagged_post_count
+                .into_iter()
+                .enumerate()
+                .for_each(|(post, count)| {
+                    if top_five.len() < 5 {
                         top_five.push((Reverse(count), post));
+                    } else {
+                        let (Reverse(cnt), _) = top_five.peek().unwrap();
+                        if count > *cnt {
+                            top_five.pop();
+                            top_five.push((Reverse(count), post));
+                        }
                     }
+                });
+
+            let related = top_five.into_iter().map(|(_, post)| &posts[post]).collect();
+
+            RelatedPosts {
+                _id: &post._id,
+                tags: &post.tags,
+                related,
+            }
+        })
+        .collect()
+}
+
+fn main() {
+    let json_str = std::fs::read_to_string("../posts.json").unwrap();
+    let posts: Vec<Post> = from_str(&json_str).unwrap();
+
+    let start = Instant::now();
+    let args: Vec<_> = std::env::args().collect();
+    match &args[..] {
+        [_progname] => {}
+        [_progname, secs] => {
+            let target = start + Duration::from_secs(secs.parse().unwrap());
+            let mut i = 0;
+            let mut now;
+            loop {
+                process(&posts);
+                i += 1;
+                now = Instant::now();
+                if now > target {
+                    break;
                 }
-            });
+            }
+            println!("{:?} per iteration", (now - start) / i);
+            return;
+        }
+        _ => panic!("invalid arguments"),
+    }
 
-        let related = top_five.into_iter().map(|(_, post)| &posts[post]).collect();
-
-        related_posts.lock().unwrap().push(RelatedPosts {
-            _id: &post._id,
-            tags: &post.tags,
-            related,
-        });
-    });
-
+    let related_posts = process(&posts);
     let end = Instant::now();
 
     print!(
@@ -91,6 +115,6 @@ fn main() {
         end.duration_since(start)
     );
 
-    let json_str = serde_json::to_string(related_posts.lock().unwrap().as_slice()).unwrap();
+    let json_str = serde_json::to_string(&related_posts).unwrap();
     std::fs::write("../related_posts_rust_rayon.json", json_str).unwrap();
 }
