0d0a01057a54d80e5f79b2219eed659b6f6613e5
jinyus <starboymc4@gmail.com>
2023-09-28 16:48:37 -0500

    add script to extra results

---
+++ extract/extract.cr
@@ -0,0 +1,17 @@
+filename = ARGV[0]
+
+if !File.exists?(filename)
+  puts "File '#{filename}' does not exist"
+  exit
+end
+
+lines = File.read_lines(filename)
+
+results = lines
+  .skip_while { |line| !line.includes? "Running Go" }
+  .map { |line| line.split(" ", 2)[1] }
+  .select { |line| line.starts_with?(/Running |Benchmark|Processing|  Time|  Range/) }
+  .map { |line| line.starts_with?("Running") ? "\n#{line.lchop("Running ")}:\n" : "\t#{line}" }
+  .join "\n"
+
+File.write("raw_results.md", "Device: Github Actions\n" + results)
