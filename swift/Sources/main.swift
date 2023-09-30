baccf68dfbb5795948a661383c47149826681891
jinyus <starboymc4@gmail.com>
2023-09-30 14:43:14 -0500

    initial

---
+++ swift/Sources/main.swift
@@ -0,0 +1,108 @@
+import Foundation
+
+class Post: Codable {
+    var id: String
+    var title: String
+    var tags: [String]
+
+    private enum CodingKeys: String, CodingKey {
+        case id = "_id"
+        case title, tags
+    }
+}
+
+class RelatedPost: Codable {
+    var id: String
+    var tags: [String]
+    var related: [Post]
+
+    init(id: String, tags: [String], related: [Post]) {
+        self.id = id
+        self.tags = tags
+        self.related = related
+    }
+
+    private enum CodingKeys: String, CodingKey {
+        case id = "_id"
+        case tags, related
+    }
+}
+
+
+let content = try String(contentsOfFile:  "../posts.json", encoding: .utf8)
+let decoder = JSONDecoder()
+let posts = try! decoder.decode([Post].self, from: content.data(using: .utf8)!)
+
+
+let t1 = Date()
+
+var tag_map = [String: [Int]]()
+for (index, post) in posts.enumerated() {
+    for tag in post.tags {
+        if tag_map[tag] == nil { tag_map[tag] = [Int]() }
+        tag_map[tag]!.append(index)
+    }
+}
+
+var allRelatedPosts = [RelatedPost]()
+var tagged_post_count = Array(repeating: 0, count: posts.count)
+
+for (idx, post) in posts.enumerated() {
+    tagged_post_count = Array(repeating: 0, count: posts.count)
+
+    for tag in post.tags {
+        for other_post_idx in tag_map[tag]! {
+            tagged_post_count[other_post_idx] += 1
+        }
+    }
+
+    tagged_post_count[idx] = 0 // don't count self
+
+    var top5Queue = Array(repeating: (0, 0), count: 6)
+    var min_tags = 0
+
+    for (idx, count) in tagged_post_count.enumerated() {
+        if count > min_tags {
+            var pos = 4
+
+            while pos >= 0 && top5Queue[pos].1 < count {
+                pos -= 1
+            }
+            pos += 1
+
+            if pos <= 4 {
+                top5Queue.insert((idx, count), at: pos)
+                min_tags = top5Queue[4].1
+            }
+        }
+    }
+
+    let topPosts = top5Queue.dropLast().map { posts[$0.0] }
+
+    allRelatedPosts.append(RelatedPost(id: post.id, tags: post.tags, related: topPosts))
+}
+
+let t2 = Date()
+
+let timeInterval = t2.timeIntervalSince(t1)
+print("Processing time (w/o IO): \(timeInterval * 1000) ms")
+
+let fileManager = FileManager.default
+let filePath = "../related_posts_swift.json"
+if !fileManager.fileExists(atPath: filePath) {
+    fileManager.createFile(atPath: filePath, contents: nil, attributes: nil)
+}
+
+
+let encoder = JSONEncoder()
+if let data = try? encoder.encode(allRelatedPosts) {
+    print("Writing \(allRelatedPosts.count) records")
+    let fileURL = URL(fileURLWithPath: filePath, isDirectory: false) 
+        do {
+            try data.write(to: fileURL, options: .atomic)
+        } catch {
+            print("An error occurred: \(error)")
+        }
+} else {
+    print("Failed to encode data")
+}
