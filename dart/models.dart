246bd4f77f0a05cb69d133038ab2614e4f68a26a
Jacob Bang <julemand101@archlinux.dk>
2023-10-02 11:45:00 +0200

    [Dart] Remove `late` in models.dart

--- dart/models.dart
+++ dart/models.dart
@@ -1,21 +1,23 @@
 class Post {
-  late String iD;
-  late String title;
-  late List<String> tags;
+  final String iD;
+  final String title;
+  final List<String> tags;
 
-  Post({required this.iD, required this.title, required this.tags});
+  Post({
+    required this.iD,
+    required this.title,
+    required this.tags,
+  });
 
-  Post.fromJson(dynamic json) {
-    iD = json['_id'];
-    title = json['title'];
-    tags = json['tags'].cast<String>();
-  }
+  factory Post.fromJson(dynamic json) => Post(
+        iD: json['_id'],
+        title: json['title'],
+        tags: List.from(json['tags']),
+      );
 
-  Map<String, dynamic> toJson() {
-    return {
-      '_id': iD,
-      'title': title,
-      'tags': tags,
-    };
-  }
+  Map<String, dynamic> toJson() => {
+        '_id': iD,
+        'title': title,
+        'tags': tags,
+      };
 }
