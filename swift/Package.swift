d3bd799e7b9099ee5bab22d534328a2a7a080e44
jinyus <starboymc4@gmail.com>
2023-10-02 12:41:36 -0500

    rm FPreview

--- swift/Package.swift
+++ swift/Package.swift
@@ -5,9 +5,9 @@ import PackageDescription
 
 let package = Package(
     name: "related",
-     dependencies: [
-        .package(url: "https://github.com/apple/swift-foundation.git",revision:"4a0637fba3cdd9c520a9ac5ab92b4c124491bd88"),
-    ],
+    //  dependencies: [
+    //     .package(url: "https://github.com/apple/swift-foundation.git",revision:"4a0637fba3cdd9c520a9ac5ab92b4c124491bd88"),
+    // ],
     targets: [
         .executableTarget(
             name: "related"
