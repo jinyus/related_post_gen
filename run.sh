61c9dce16f5e077b4c97407b4b98ff022c693c3c
jinyus <starboymc4@gmail.com>
2023-10-01 21:27:01 -0500

    add to all runner

--- run.sh
+++ run.sh
@@ -351,6 +351,7 @@ elif [ "$first_arg" = "all" ]; then
         run_swift || echo -e "\n" &&
         run_js "node" || echo -e "\n" &&
         run_js "bun" || echo -e "\n" &&
+        run_js "deno" || echo -e "\n" &&
         echo -e "Finished running all\n"
 
 elif [ "$first_arg" = "clean" ]; then
