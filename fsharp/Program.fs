dcea81272124af4a02d8fb8620fa1f28809ac254
jinyus <starboymc4@gmail.com>
2023-10-03 17:52:48 -0500

    working

--- fsharp/Program.fs
+++ fsharp/Program.fs
@@ -22,14 +22,18 @@ let posts = Json.deserialize<Post[]> (File.ReadAllText "../posts.json")
 
 let start = DateTime.Now
 
-let tagMap: Map<string, int array> =
+// let tagMap: Map<string, int array> =
+let tagMap =
     posts
-    |> Array.collect (fun p -> p.tags |> Array.mapi (fun i t -> t, [| i |]))
+    |> Array.mapi (fun i p -> i, p)
+    |> Array.collect (fun (i, p) -> p.tags |> Array.map (fun t -> t, [| i |]))
     |> Array.groupBy fst
     |> Array.map (fun (t, is) -> t, is |> Array.collect snd)
     |> Map.ofArray
 
+// printf "tagmap took: %dms" (DateTime.Now - start).Milliseconds
 
+// File.WriteAllText("../tag_map_fsharp.json", Json.serialize tagMap)
 
 let mutable taggedPostCount = Array.create posts.Length 0
 
@@ -41,18 +45,19 @@ let allRelatedPosts: RelatedPosts[] =
         Array.fill taggedPostCount 0 posts.Length 0
 
         for tag in post.tags do
-            for oIDX in tagMap.[tag] do
-                taggedPostCount.[oIDX] <- taggedPostCount.[oIDX] + 1
+            for oIDX in tagMap[tag] do
+                taggedPostCount[oIDX] <- taggedPostCount[oIDX] + 1
 
 
-        taggedPostCount.[i] <- 0
+        taggedPostCount[i] <- 0
 
         let topN = 5
         let mutable top5 = Array.zeroCreate (topN * 2) // flattened list of (count, id)
         let mutable minTags = 0
 
-        taggedPostCount
-        |> Array.iteri (fun i count ->
+        for i in 0 .. taggedPostCount.Length - 1 do
+            let count = taggedPostCount.[i]
+
             if count > minTags then
                 // Find upper bound: pos at which count is larger than current one.
                 let mutable upperBound = (topN - 2) * 2
@@ -67,9 +72,6 @@ let allRelatedPosts: RelatedPosts[] =
                 top5.[insertionPos + 1] <- i
 
                 minTags <- top5.[topN * 2 - 2]
-            |> ignore
-
-        )
 
         { _id = post._id
           tags = post.tags
