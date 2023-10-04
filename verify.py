7ebd8552b871badf7b4ab67a4a8ff983b4e22a18
jinyus <starboymc4@gmail.com>
2023-10-03 19:44:50 -0500

    color errors

--- verify.py
+++ verify.py
@@ -3,6 +3,25 @@ import sys
 from typing import Dict, List
 
 
+def text_color(color, text):
+    colors = {
+        "red": "\033[31m",
+        "green": "\033[32m",
+        "yellow": "\033[33m",
+        "blue": "\033[34m",
+        "magenta": "\033[35m",
+        "cyan": "\033[36m",
+        "white": "\033[37m",
+    }
+
+    return f"{colors[color]}{text}\x1B[0m"
+
+
+ERROR = text_color("red", "Error:")
+WARNING = text_color("yellow", "Warning:")
+SUCCESS = text_color("green", "Success:")
+
+
 def generate_correct_related_posts():
     with open("./related_posts_python.json") as f:
         python_related: List[Dict] = json.load(f)
@@ -45,19 +64,18 @@ if __name__ == "__main__":
 
     if len(target_file) != len(correct_file):
         print(
-            f"Invalid post count! expected: {len(correct_file)} actual: {len(target_file)}"
+            f"{WARNING} Invalid post count! \nexpected: {len(correct_file)} \nactual: {len(target_file)}\n"
         )
-        sys.exit(1)
 
     for post in target_file:
         if "_id" not in post:
-            print(f"Invalid post! _id not found: \n{post}")
+            print(f"{ERROR} Invalid post! _id not found: \n{post}")
             sys.exit(1)
         elif "tags" not in post:
-            print(f"Invalid post! tags not found: \n{post}")
+            print(f"{ERROR} Invalid post! tags not found: \n{post}")
             sys.exit(1)
         elif "related" not in post:
-            print(f"Invalid post! related not found: \n{post}")
+            print(f"{ERROR} Invalid post! related not found: \n{post}")
             sys.exit(1)
 
         tag_set = set(post["tags"])
@@ -69,16 +87,16 @@ if __name__ == "__main__":
         post_id = post["_id"]
 
         if post_id not in correct_file:
-            print(f"Post {post_id} not found in correct file!\n{post}")
+            print(f"{ERROR} Post {post_id} not found in correct file!\n{post}")
             sys.exit(1)
 
         correct_count = correct_file[post_id]
 
         if shared_tag_count != correct_count:
             print(
-                f"Post {post_id} is invalid!\nexpected: {correct_count} shared tag count\nactual: {shared_tag_count}"
+                f"{ERROR} Post {post_id} is invalid!\nexpected: {correct_count} shared tag count\nactual: {shared_tag_count}"
             )
             sys.exit(1)
 
-    print(f"Output file: {filepath} is valid!")
+    print(f"{SUCCESS} {filepath} is valid!")
     sys.exit(0)
