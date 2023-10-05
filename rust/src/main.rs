87fdcd9106ff8b4a5e8f166656f55cf31ed1965c
Mike Cronce <mike@quadra-tec.net>
2023-10-04 23:49:20 -0400

    rust/src/main.rs:  #[inline] trivial functions

--- rust/src/main.rs
+++ rust/src/main.rs
@@ -29,18 +29,21 @@ struct PostCount {
 }
 
 impl std::cmp::PartialEq for PostCount {
+    #[inline]
     fn eq(&self, other: &Self) -> bool {
         self.count == other.count
     }
 }
 
 impl std::cmp::PartialOrd for PostCount {
+    #[inline]
     fn partial_cmp(&self, other: &Self) -> Option<std::cmp::Ordering> {
         Some(self.cmp(other))
     }
 }
 
 impl std::cmp::Ord for PostCount {
+    #[inline]
     fn cmp(&self, other: &Self) -> std::cmp::Ordering {
         // reverse order
         other.count.cmp(&self.count)
