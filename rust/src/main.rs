2ed7b8fad1847fa606bc41a99485b2b15da3f072
Mike Cronce <mike@quadra-tec.net>
2023-10-05 00:47:35 -0400

    rust:  Bring back mimalloc

--- rust/src/main.rs
+++ rust/src/main.rs
@@ -4,6 +4,10 @@ use rustc_data_structures::fx::FxHashMap;
 use serde::{Deserialize, Serialize};
 use serde_json::from_str;
 
+use mimalloc::MiMalloc;
+#[global_allocator]
+static GLOBAL: MiMalloc = MiMalloc;
+
 type SString = smallstr::SmallString<[u8; 16]>;
 
 #[derive(Serialize, Deserialize)]
