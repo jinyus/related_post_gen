7e2e75c8be5be32fe2ee56ab91347eed3624e5cd
jinyus <starboymc4@gmail.com>
2023-10-05 07:23:40 -0500

    init

--- js/related.js
+++ js/related.js
@@ -1,4 +1,6 @@
 
+const topN = 5;
+
 export function genRelatedPosts(posts) {
   // build a map of tags to post indices. tag -> [idx1, idx2, ...]
   const tagMap = {};
@@ -17,49 +19,54 @@ export function genRelatedPosts(posts) {
   return Array(posts.length)
     .fill({})
     .map((record, i) => {
-    
-    taggedPostCount.fill(0);
-    let post = posts[i];
 
-    for (const tag of post.tags) {
-      for (const otherIdx of tagMap[tag]) {
-        taggedPostCount[otherIdx]++;
-      }
-    }
+      taggedPostCount.fill(0);
+      let post = posts[i];
 
-    taggedPostCount[i] = 0; // exclude self
-
-    let top5 = Array(5).fill({
-      idx: 0,
-      count: 0,
-    });
-
-    let minTags = 0;
-
-    // custom priority queue to find top 5
-    taggedPostCount.forEach((count, i2) => {
-
-      if (count > minTags) {
-        let pos = 4;
-
-        while (pos >= 0 && count > top5[pos].count) {
-          pos--;
+      for (const tag of post.tags) {
+        for (const otherIdx of tagMap[tag]) {
+          taggedPostCount[otherIdx]++;
         }
-
-        pos++;
-        top5.splice(pos, 0, { idx: i2, count });
-        top5.pop();
-        minTags = top5[4].count;
       }
 
+      taggedPostCount[i] = 0; // exclude self
+
+      let top5 = Array(topN * 2).fill(0);  // flattened list of (count, id)
+      let minTags = 0;
+
+      for (let j in taggedPostCount) {
+        let count = taggedPostCount[j];
+        if (count > minTags) {
+
+          let upperBound = (topN - 2) * 2;
+
+          while (upperBound >= 0 && count > top5[upperBound]) {
+            top5[upperBound + 2] = top5[upperBound];
+            top5[upperBound + 3] = top5[upperBound + 1];
+            upperBound -= 2;
+          }
+
+          let insertPos = upperBound + 2;
+          top5[insertPos] = count;
+          top5[insertPos + 1] = j;
+
+          minTags = top5[topN * 2 - 2];
+        }
+      }
+
+      let topPosts = Array(topN).fill(null);
+
+      // Convert indexes back to Post pointers
+      for (let i = 1; i < 10; i += 2) {
+        topPosts[Math.floor(i / 2)] = posts[top5[i]];
+      }
+
+      record._id = post._id;
+      record.title = post.title;
+      record.tags = post.tags;
+      record.related = topPosts;
+
+      return record;
     });
-
-    record._id = post._id;
-    record.title = post.title;
-    record.tags = post.tags;
-    record.related = top5.map((p) => posts[p.idx]);
-
-    return record;
-  });
 }
 
