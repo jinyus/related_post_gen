56a19fb5e2f9426663bc1e8d060be491e4ea9305
Ivan <fizmat.r66@gmail.com>
2023-10-07 01:08:24 +0400

    combine JIT functions for faster compile and minor performance boost

--- python/related_numba.py
+++ python/related_numba.py
@@ -7,38 +7,29 @@ from numba import njit, typed
 
 
 @njit
-def count_relations(n_posts, t_to_pp, p_to_tt, p_to_nt, current_p):
-    tt = p_to_tt[current_p]
-    relation_count = np.zeros(n_posts, dtype=np.uint8)
-    for tj in range(p_to_nt[current_p]):
-        for p in t_to_pp[tt[tj]]:
-            relation_count[p] += 1
-    relation_count[current_p] = 0
-    return relation_count
-
-
-@njit
-def get_top5(relation_count):
-    max_i = np.zeros(5, dtype=np.uint16)
-    max_r = np.zeros(5, dtype=np.uint8)
-    for i, r in enumerate(relation_count):
-        for j in range(4, -1, -1):
-            if r > max_r[j]:
-                max_r[:j] = max_r[1:j + 1]
-                max_i[:j] = max_i[1:j + 1]
-                max_r[j] = r
-                max_i[j] = i
-                break
-    return max_i
-
-
-@njit
 def get_all_top5(n_posts, t_to_pp, p_to_tt, p_to_nt):
     all_top5 = np.empty((n_posts, 5), np.uint16)
-    for p in range(n_posts):
-        relation_count = count_relations(n_posts, t_to_pp, p_to_tt, p_to_nt, p)
-        top5 = get_top5(relation_count)
-        all_top5[p] = top5
+    for current_p in range(n_posts):
+        tt = p_to_tt[current_p]
+        relation_count = np.zeros(n_posts, dtype=np.uint8)
+        for tj in range(p_to_nt[current_p]):
+            for p in t_to_pp[tt[tj]]:
+                relation_count[p] += 1
+        relation_count[current_p] = 0
+
+        # get top 5:
+        # (probably can be optimized more)
+        max_i = np.zeros(5, dtype=np.uint16)
+        max_r = np.zeros(5, dtype=np.uint8)
+        for i, r in enumerate(relation_count):
+            for j in range(4, -1, -1):
+                if r > max_r[j]:
+                    max_r[:j] = max_r[1:j + 1]
+                    max_i[:j] = max_i[1:j + 1]
+                    max_r[j] = r
+                    max_i[j] = i
+                    break
+        all_top5[current_p] = max_i
     return all_top5
 
 
@@ -47,13 +38,6 @@ def precompile():
     # JIT compile by running with the arguments of the correct type
     # 1) measure compile time
     # 2) get correct processing (without compilation) time using the cached machine code
-    count_relations(
-        1,
-        typed.List([np.empty(0, dtype=np.uint16)]),
-        np.empty((1, 0), dtype=np.uint8),
-        np.zeros(1, dtype=np.uint8),
-        0)
-    get_top5(np.empty(0, dtype=np.uint8))
     get_all_top5(
         0,
         typed.List([np.empty(0, dtype=np.uint16)]),
