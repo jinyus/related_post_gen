68b93f19901a800568dc2c05c113f2f0962a3260
mvikto <mviktolive@gmail.com>
2023-10-06 21:17:41 +0300

    add Dockerfile

--- ocaml/bin/main.ml
+++ ocaml/bin/main.ml
@@ -17,10 +17,11 @@ type related_posts =
 type post_array = post array [@@deriving of_yojson]
 type related_posts_array = related_posts array [@@deriving to_yojson]
 
-let source = "../posts.json"
-let destination = "../related_posts_ocaml.json"
-
 let () =
+  let args = Sys.get_argv () in
+  Array.sexp_of_t String.sexp_of_t args |> Sexp.to_string |> Stdio.print_endline;
+  let source = args.(1) in
+  let destination = args.(2) in
   let posts =
     Yojson.Safe.from_file source |> post_array_of_yojson |> Result.ok_or_failwith
   in
@@ -39,7 +40,7 @@ let () =
   let allrelatedPosts =
     Array.mapi posts ~f:(fun postId post ->
       let taggedPostCount = Array.create ~len:(Array.length posts) 0 in
-      let top5 = Array.create_local ~len:(topN * 2) 0 in
+      let top5 = Array.create ~len:(topN * 2) 0 in
       Array.iter post.tags ~f:(fun tagId ->
         Array.iter (Hashtbl.find_exn tagPosts tagId) ~f:(fun relatedPostId ->
           taggedPostCount.(relatedPostId) <- taggedPostCount.(relatedPostId) + 1));
