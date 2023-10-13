5f08c5bff97d5fe2d0a3b9e6b2a1f97ee1db6c23
inv2004 <inv2004@gmail.com>
2023-10-13 18:17:41 +0300

    [Nim] fix std/table lent

--- nim/src/related.nim
+++ nim/src/related.nim
@@ -1,6 +1,8 @@
-import std/[tables, hashes, sequtils, monotimes, times]
+import std/[hashes, monotimes, times]
+import std/sequtils
 import jsony
 import xxhash
+import fixedtable
 
 type
   Post = ref object
@@ -8,68 +10,76 @@ type
     title: string
     tags : seq[string]
 
-  RelatedPosts = ref object
+  RelatedPosts = object
     `"_id"`: string
     tags : seq[string]
     related : seq[Post]
-
-  Key = string
-
-proc hash(x: Key): Hash =
-  cast[Hash](XXH3_64bits(x))
 
 const
     input = "../posts.json"
     output = "../related_posts_nim.json"
 
-proc main() =
-  let posts = readFile(input).fromJson(seq[Post])
+proc hash(x: string): Hash =
+  cast[Hash](XXH3_64bits(x))
 
-  let start = getMonotime()
+proc findTop5(posts: seq[Post], taggedPostCount: seq[uint8]): seq[Post] =
+  result = newSeq[Post](5)
+  var top5: array[5, tuple[idx: int, count: uint8]]
+  var min = 0
+  for i, count in taggedPostCount:
+    if count > top5[4].count:
+      top5[4] = (idx: i, count: count)
+      for pos in countdown(3, 0):
+        if count > top5[pos].count:
+          swap(top5[pos+1], top5[pos])
+  for i, t in top5:
+    result[i] = posts[t.idx]
 
-  var tagMap: Table[Key, seq[int]]
+proc genTagMap(posts: seq[Post]): Table[string, seq[int]] =
+  result = initTable[string, seq[int]](100)
   for i, post in posts:
     for tag in post.tags:
-      if tagMap.hasKeyOrPut(tag.Key, @[i]):
-        tagMap[tag.Key].add i
+      result.withValue(tag, val):
+        val[].add i
+      do:
+        result[tag] = @[i]
+
+proc countTaggedPost(taggedPostCount: var seq[uint8], i: int, post: Post, tagMap: Table[string, seq[int]]) =
+  for tag in post.tags:
+    for otherIDX in tagMap[tag]:
+      inc(taggedPostCount[otherIDX])
+  taggedPostCount[i] = 0 # remove self
+
+proc process(posts: seq[Post]): seq[RelatedPosts] =
+  let start = getMonotime()
+
+  let tagMap = genTagMap(posts)
 
   var allRelatedPosts = newSeq[RelatedPosts](posts.len)
   var taggedPostCount = newSeq[uint8](posts.len)
 
   for i, post in posts.pairs:
-    zeroMem(taggedPostCount[0].addr, taggedPostCount.len)
+    zeroMem(taggedPostCount[0].addr, posts.len)
+    taggedPostCount.countTaggedPost(i, post, tagMap)
 
-    for tag in post.tags:
-      for otherIDX in tagMap[tag.Key]:
-        taggedPostCount[otherIDX] += 1
-
-    taggedPostCount[i] = 0 # remove self
-
-    var top5: array[5, tuple[idx: int, count: uint8]]
-
-    for i, count in taggedPostCount:
-      if count > top5[4].count:
-        top5[4] = top5[3]
-        block loop:
-          for pos in countdown(3, 1):
-            if count > top5[pos - 1].count:
-              top5[pos] = top5[pos - 1]
-            else:
-              top5[pos] = (idx: i, count: count)
-              break loop
-          top5[0] = (idx: i, count: count)
-
-    allRelatedPosts[i] = RelatedPosts(
+    let relatedPosts = RelatedPosts(
       `"_id"`: post.`"_id"`,
       tags: post.tags,
-      related: top5.map(proc(x: auto): Post = posts[x.idx]),
+      related: posts.findTop5(taggedPostCount)
       )
+
+    allRelatedPosts[i] = relatedPosts
 
   let total = getMonotime() - start
 
-  echo "Processing time (w/o IO): ", total.inMilliseconds, "ms"
+  echo "Processing time (w/o IO): ", total.inMicroseconds / 1000, "ms"
 
-  writeFile(output, allRelatedPosts.toJson)
+  return allRelatedPosts
+
+proc main() =
+  let posts = readFile(input).fromJson(seq[Post])
+  let res = process(posts)
+  writeFile(output, res.toJson)
 
 when isMainModule:
   main()
