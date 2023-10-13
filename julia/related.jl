24260014c05e5bc8e24a450deab7daee5d690583
Syx Pek <20557013+SyxP@users.noreply.github.com>
2023-10-13 21:40:31 +0800

    Improve Speed

--- julia/related.jl
+++ julia/related.jl
@@ -9,10 +9,10 @@ function relatedIO()
     json_string = read("../posts.json", String)
     posts = JSON3.read(json_string, Vector{PostData})    
 
-    related(posts)
+    fasterrelated(posts)
 
     start = now()
-    all_related_posts = related(posts)
+    all_related_posts = fasterrelated(posts)
     println("Processing time (w/o IO): $(now() - start)")
 
 
@@ -35,75 +35,78 @@ end
 
 StructTypes.StructType(::Type{PostData}) = StructTypes.Struct()
 
-function fastmaxindex!(xs::Vector, topn, maxn, maxv)
+function fastermaxindex!(tagmask::Vector{T}, i, topn, maxn, maxv) where T
     maxn .= 1
     maxv .= 0
-    top = maxv[1]
-    for (i, x) in enumerate(xs)
-        if x > top
-            maxv[1] = x
-            maxn[1] = i
-            for j in 2:topn
-                if maxv[j-1] > maxv[j]
-                    maxv[j-1], maxv[j] = maxv[j], maxv[j-1]
-                    maxn[j-1], maxn[j] = maxn[j], maxn[j-1]
-                end
+    req = 0
+
+    A = tagmask[i]
+
+    for (j, mask) in enumerate(tagmask)
+        i == j && continue
+        x = count_ones(A & mask)
+        x > req || continue
+        idx = topn 
+        u = maxv[idx - 1]
+        req = min(u, x)
+        while x > u
+            maxv[idx] = u
+            maxn[idx] = maxn[idx - 1]
+            idx -= 1
+            idx == 1 && break
+            u = maxv[idx - 1]
+        end
+
+        maxv[idx] = x
+        maxn[idx] = j
+    end
+
+    maxn    
+end
+
+for Container in [UInt8, UInt16, UInt32, UInt64, UInt128]
+    @eval function getrelatedposts!(tagmap, posts, topn, maxn, maxv, relatedposts, ::Val{$Container})
+        tagmask = zeros($Container, length(posts))
+        for (idx, post) in enumerate(posts)
+            for tag in post.tags
+                tagmask[idx] |= one($Container) << tagmap[tag]
             end
-            top = maxv[1]
         end
-    end
 
-    reverse!(maxn)
-
-    return maxn
-end
-
-function related(posts)
-    for T in (UInt8, UInt16, UInt32, UInt64)
-        if length(posts) < typemax(T)
-            return related(T, posts)
+        for (i, post) in enumerate(posts)
+            fastermaxindex!(tagmask, i, topn, maxn, maxv)
+            relatedpost = RelatedPost(post._id, post.tags, SVector{topn}(@view posts[maxn]))
+            relatedposts[i] = relatedpost
         end
+
+        return
     end
 end
-function related(::Type{T}, posts) where {T}
-    topn = 5
-    # key is every possible "tag" used in all posts
-    # value is indicies of all "post"s that used this tag
-    tagmap = Dict{String,Vector{T}}()
+
+function getContainer(x)
+    for (y, size) in zip([UInt8, UInt16, UInt32, UInt64], [8, 16, 32, 64])
+        x < size && return Val{y}()
+    end
+    Val{UInt128}()
+end
+
+function fasterrelated(posts)
+    tagmap = Dict{String, Int}()
+
+    Ct = 0
     for (idx, post) in enumerate(posts)
         for tag in post.tags
-            tags = get!(() -> T[], tagmap, tag)
-            push!(tags, idx)
+            get!(() -> (Ct += 1), tagmap, tag)
         end
     end
-
+    
+    topn = 5
+    maxn = Vector{Int}(undef, 5)
+    maxv = Vector{Int}(undef, 5)
+    
     relatedposts = Vector{RelatedPost}(undef, length(posts))
-    taggedpostcount = Vector{T}(undef, length(posts))
-
-    maxn = Vector{T}(undef,topn)
-    maxv = Vector{T}(undef,topn)
-
-    for (i, post) in enumerate(posts)
-        taggedpostcount .= 0
-        # for each post (`i`-th)
-        # and every tag used in the `i`-th post
-        # give all related post +1 in `taggedpostcount` shadow vector
-        for tag in post.tags
-            for idx in tagmap[tag]
-                taggedpostcount[idx] += one(T)
-            end
-        end
-
-        # don't self count
-        taggedpostcount[i] = 0
-
-        fastmaxindex!(taggedpostcount, topn, maxn, maxv)
-
-        relatedpost = RelatedPost(post._id, post.tags, SVector{topn}(@view posts[maxn]))
-        relatedposts[i] = relatedpost
-    end
-
-    return relatedposts
+    getrelatedposts!(tagmap, posts, topn, maxn, maxv, relatedposts, getContainer(Ct))
+    relatedposts
 end
 
 const res = relatedIO()
