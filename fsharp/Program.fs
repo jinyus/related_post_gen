43a884d5016e84b982b86fad1676c3c7c38229a3
jinyus <starboymc4@gmail.com>
2023-10-13 07:58:00 -0500

    minor grammar fix

--- fsharp/Program.fs
+++ fsharp/Program.fs
@@ -2,7 +2,7 @@
 open System.IO
 open FSharp.NativeInterop
 open System.Collections.Generic
-open FSharp.Json //System.Text.Json not aot friendly
+open FSharp.Json //System.Text.Json is not aot friendly
 
 #nowarn "9"
 
