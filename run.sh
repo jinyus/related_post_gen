e8c722995d5d5beb6499b567fae36784c10ec493
Kai WU <kaiwu2004@gmail.com>
2023-10-15 00:38:38 +0800

    [cpp con] add cpp concurrent

--- run.sh
+++ run.sh
@@ -86,19 +86,6 @@ run_go_concurrent() {
     check_output "related_posts_go_con.json"
 }
 
-run_rust() {
-    echo "Running Rust" &&
-        cd ./rust &&
-        cargo build --release &&
-        if [ $HYPER == 1 ]; then
-            capture "Rust" hyperfine -r $runs -w $warmup --show-output "./target/release/rust"
-        else
-            command ${time} -f '%es %Mk' ./target/release/rust
-        fi
-
-    check_output "related_posts_rust.json"
-}
-
 run_cpp() {
     echo "Running C++" &&
         cd ./cpp &&
@@ -110,6 +97,32 @@ run_cpp() {
         fi
 
     check_output "related_posts_cpp.json"
+}
+
+run_cpp_con() {
+    echo "Running C++ Concurrent" &&
+        cd ./cpp &&
+        g++ -std=c++11 -pthread -I./include -O3 main.cpp -o main &&
+        if [ $HYPER == 1 ]; then
+            capture "cpp" hyperfine -r $runs -w $warmup --show-output "./main"
+        else
+            command ${time} -f '%es %Mk' ./main
+        fi
+
+    check_output "related_posts_cpp_con.json"
+}
+
+run_rust() {
+    echo "Running Rust" &&
+        cd ./rust &&
+        cargo build --release &&
+        if [ $HYPER == 1 ]; then
+            capture "Rust" hyperfine -r $runs -w $warmup --show-output "./target/release/rust"
+        else
+            command ${time} -f '%es %Mk' ./target/release/rust
+        fi
+
+    check_output "related_posts_rust.json"
 }
 
 run_rust_con() {
@@ -598,6 +611,10 @@ elif [ "$first_arg" = "go_con" ]; then
 elif [ "$first_arg" = "cpp" ]; then
 
     run_cpp
+
+elif [ "$first_arg" = "cpp_con" ]; then
+
+    run_cpp_con
 
 elif [ "$first_arg" = "rust" ]; then
 
