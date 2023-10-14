c488c0fd95e2b966cb6e8b5cb3c2099e9c36ffbe
jinyus <starboymc4@gmail.com>
2023-10-14 14:18:56 -0500

    restructure f# con to match singlecore version

--- fsharp_con/Program.fs
+++ fsharp_con/Program.fs
@@ -1,8 +1,8 @@
 ﻿open System
 open System.IO
 open FSharp.NativeInterop
-open FSharp.Json
 open System.Collections.Generic
+open FSharp.Json //System.Text.Json is not aot friendly
 
 #nowarn "9"
 
@@ -10,94 +10,99 @@ let inline stackalloc<'a when 'a: unmanaged> (length: int) : Span<'a> =
     let p = NativePtr.stackalloc<'a> length |> NativePtr.toVoidPtr
     Span<'a>(p, length)
 
-
-[<Struct>]
 type Post =
     { _id: string
       title: string
       tags: string[] }
 
-[<Struct>]
 type RelatedPosts =
     { _id: string
       tags: string[]
       related: Post[] }
 
+[<Literal>]
 let srcDir = __SOURCE_DIRECTORY__
-let posts = Json.deserialize<Post[]> (File.ReadAllText $"{srcDir}/../posts.json")
 
-let stopwatch = Diagnostics.Stopwatch()
-stopwatch.Start()
-
-// Start work
-let tagPostsTmp = Dictionary<string, Stack<int>>()
-
-posts
-|> Array.iteri (fun postId post ->
-
-    for tag in post.tags do
-
-        match tagPostsTmp.TryGetValue tag with
-        | true, s -> s.Push postId
-        | false, _ ->
-            let newStack = Stack()
-            newStack.Push postId
-            tagPostsTmp[tag] <- newStack)
-
-// convert from Dict<_,Stack<int>> to Dict<_,int[]> for faster access
-let tagPosts = Dictionary(tagPostsTmp.Count)
-for kv in tagPostsTmp do
-    tagPosts[kv.Key] <- kv.Value.ToArray() 
-
+[<Literal>]
 let topN = 5
 
-let allRelatedPosts: RelatedPosts[] =
+
+let getAllRelated (posts: Post[]) =
+    // Start work
+    let tagPostsTmp = Dictionary<string, Stack<int>>()
+
     posts
-    |> Array.Parallel.mapi (fun postId post ->
+    |> Array.iteri (fun postId post ->
+
+        for tag in post.tags do
+
+            match tagPostsTmp.TryGetValue tag with
+            | true, s -> s.Push postId
+            | false, _ ->
+                let newStack = Stack()
+                newStack.Push postId
+                tagPostsTmp[tag] <- newStack)
+
+    // convert from Dict<_,Stack<int>> to Dict<_,int[]> for faster access
+    let tagPosts = Dictionary(tagPostsTmp.Count)
+
+    for kv in tagPostsTmp do
+        tagPosts[kv.Key] <- kv.Value.ToArray()
+
+
+    let getRelated (idx: int) (post: Post) =
         let taggedPostCount = stackalloc posts.Length
-        let top5 = stackalloc (topN * 2) // flattened list of (count, id)
 
         for tagId in post.tags do
             for relatedPostId in tagPosts[tagId] do
-                taggedPostCount[relatedPostId] <- taggedPostCount[relatedPostId] + 1
+                let mutable relatedPostTagCount = &taggedPostCount[relatedPostId]
+                relatedPostTagCount <- relatedPostTagCount + 1uy
 
-        taggedPostCount[postId] <- 0 // ignore self
+        taggedPostCount[idx] <- 0uy // ignore self
 
-        let mutable minTags = 0
+        let top5 = Array.zeroCreate<struct {| count: byte; postId: int |}> topN
+        let mutable minTags = 0uy
 
-        // custom priority queue to find topN
         for i in 0 .. taggedPostCount.Length - 1 do
             let count = taggedPostCount[i]
 
             if count > minTags then
                 // Find upper bound: pos at which count is larger than current one.
-                let mutable upperBound = (topN - 2) * 2
+                let mutable pos = topN - 2
 
-                while upperBound >= 0 && count > top5[upperBound] do
-                    top5[upperBound + 2] <- top5[upperBound]
-                    top5[upperBound + 3] <- top5[upperBound + 1]
-                    upperBound <- upperBound - 2
+                while pos >= 0 && count > top5[pos].count do
+                    top5[pos + 1] <- top5[pos]
+                    pos <- pos - 1
 
-                let insertionPos = upperBound + 2
-                top5[insertionPos] <- count
-                top5[insertionPos + 1] <- i
+                top5[pos + 1] <- {| count = count; postId = i |}
+                minTags <- top5[topN - 1].count
 
-                minTags <- top5[topN * 2 - 2]
-
-        let related = Array.zeroCreate topN
-
-        for i in 0 .. related.Length - 1 do
-            related[i] <- posts[top5[i * 2 + 1]]
 
         { _id = post._id
           tags = post.tags
-          related = related }
-
-    )
+          related = top5 |> Array.map (fun top -> posts[top.postId]) }
 
 
-stopwatch.Stop()
-printfn "Processing time (w/o IO): %dms" stopwatch.ElapsedMilliseconds
-let json = Json.serialize allRelatedPosts
+    posts |> Array.Parallel.mapi getRelated
 
-File.WriteAllText($"{srcDir}/../related_posts_fsharp_con.json", json)
+
+
+
+[<EntryPoint>]
+let main args =
+    let posts = Json.deserialize<Post[]> (File.ReadAllText $"{srcDir}/../posts.json")
+
+    // Warmup
+    getAllRelated posts |> ignore
+
+    let stopwatch = Diagnostics.Stopwatch.StartNew()
+
+    let allRelatedPosts = getAllRelated posts
+
+    stopwatch.Stop()
+    Console.WriteLine($"Processing time (w/o IO): %d{stopwatch.ElapsedMilliseconds}ms")
+
+    let json = Json.serialize allRelatedPosts
+
+    File.WriteAllText($"{srcDir}/../related_posts_fsharp.json", json)
+    0
