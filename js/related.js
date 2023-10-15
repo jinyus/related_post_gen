eac5c8c16a469f19e5bd4de447f3091fc9d3da93
jinyus <starboymc4@gmail.com>
2023-10-15 14:05:07 -0500

    replace forEach and for in with reg. for loops.
    co-authored-by: morkai <morkai@users.noreply.github.com>

--- js/related.js
+++ js/related.js
@@ -1,48 +1,53 @@
 export function genRelatedPosts(posts) {
+  const postsCount = posts.length;
   // build a map of tags to post indices. tag -> [idx1, idx2, ...]
   const tagMap = {};
-  posts.forEach((post, i) => {
-    for (const tag of post.tags) {
+  for (let i = 0; i < postsCount; i++) {
+    const post = posts[i];
+    const tagsCount = post.tags.length;
+    for (let j = 0; j < tagsCount; j++) {
+      const tag = post.tags[j];
       if (tagMap[tag]) {
         tagMap[tag].push(i);
       } else {
         tagMap[tag] = [i];
       }
     }
-  });
+  }
 
-  const postsCount = posts.length;
-  const taggedPostCount = Array(postsCount);
-  const allRelated = Array(postsCount);
+  const taggedPostCount = new Array(postsCount);
+  const allRelated = new Array(postsCount);
 
   for (let i = 0; i < postsCount; i++) {
-    let record = {};
-
     // fill array with 0s manually. Array.fill() is slow in bun
     for (let j = 0; j < postsCount; j++) {
       taggedPostCount[j] = 0;
     }
 
-    let post = posts[i];
+    const post = posts[i];
+    const tagsCount = post.tags.length;
 
-    for (const tag of post.tags) {
-      for (const otherIdx of tagMap[tag]) {
-        taggedPostCount[otherIdx]++;
+    for (let j = 0; j < tagsCount; j++) {
+      const other = tagMap[post.tags[j]];
+      const otherCount = other.length;
+      for (let k = 0; k < otherCount; k++) {
+        taggedPostCount[other[k]]++;
       }
     }
 
     taggedPostCount[i] = 0; // exclude self
 
-    let top5 = Array(5);
-
+    const top5 = new Array(5);
     for (let j = 0; j < 5; j++) {
-      top5[j] = { idx: -1, count: 0 };
+      top5[j] = { post: null, count: 0 };
     }
 
     let minTags = 0;
 
     // custom priority queue to find top 5
-    taggedPostCount.forEach((count, i2) => {
+    for (let j = 0; j < postsCount; j++) {
+      const count = taggedPostCount[j];
+
       if (count > minTags) {
         let pos = 4;
 
@@ -51,18 +56,18 @@ export function genRelatedPosts(posts) {
         }
 
         pos++;
-        top5.splice(pos, 0, { idx: i2, count });
+        top5.splice(pos, 0, { post: posts[j], count });
         top5.pop();
         minTags = top5[4].count;
       }
-    });
+    }
 
-    record._id = post._id;
-    record.title = post.title;
-    record.tags = post.tags;
-    record.related = top5.map((p) => posts[p.idx]);
-
-    allRelated[i] = record;
+    allRelated[i] = {
+      _id: post._id,
+      title: post.title,
+      tags: post.tags,
+      related: top5.map(p => p.post)
+    };
   }
 
   return allRelated;
