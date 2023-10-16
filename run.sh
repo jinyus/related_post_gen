73da9dc02f0bff5083032252493aa7bd479711c8
Nikita Vorontsov <nikita.vorontsov@engee.cloud>
2023-10-16 21:04:01 +0300

    enable erlang to run_all

--- run.sh
+++ run.sh
@@ -883,7 +883,7 @@ elif [ "$first_arg" = "all" ]; then
         run_luajit || echo -e "\n" &&
         run_lua || echo -e "\n" &&
         run_ocaml || echo -e "\n" &&
-        # run_erlang || echo -e "\n" && # erlang is too slow right now
+        run_erlang || echo -e "\n" && # now it's fast
         echo -e "Finished running all\n"
 
 elif [ "$first_arg" = "clean" ]; then
