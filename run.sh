6534bb8a97d38cd5a21743d75439395d85da2322
jinyus <starboymc4@gmail.com>
2023-10-16 18:50:45 -0500

    rm comment

--- run.sh
+++ run.sh
@@ -929,7 +929,7 @@ elif [ "$first_arg" = "all" ]; then
         run_luajit || echo -e "\n" &&
         run_lua || echo -e "\n" &&
         run_ocaml || echo -e "\n" &&
-        run_erlang || echo -e "\n" && # now it's fast
+        run_erlang || echo -e "\n" &&
         echo -e "Finished running all\n"
 
 elif [ "$first_arg" = "clean" ]; then
