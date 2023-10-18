8c319a4b615ca55fe2daed4119dec8d6af8c18a2
Serg Gini <kornburn@gmail.com>
2023-10-19 00:48:16 +0300

    [D] cleaned version
    Same performance. Just cleaned code after @schveiguy review

--- d_con/source/app.d
+++ d_con/source/app.d
@@ -2,9 +2,7 @@ import std.stdio : writeln, toFile;
 import std.datetime.stopwatch : StopWatch, AutoStart;
 import std.file : readText;
 import std.parallelism : taskPool, parallel;
-import core.simd : Vector;
 import ldc.simd;
-import std.algorithm : max;
 import asdf.serialization : deserialize, serializeToJson;
 
 enum TopN = 5;
@@ -31,9 +29,7 @@ struct PostsWithSharedTags
 
 enum vSize = 16;
 alias hashmap = size_t[][string];
-alias vec_u = Vector!(ubyte[vSize]);
-
-
+alias vec_u = __vector(ubyte[vSize]);
 
 void main()
 {
@@ -57,10 +53,9 @@ void main()
 	auto nVectors = (postsCount + vSize - 1) / vSize;
 	auto taggedPostsVecThreadPool = taskPool.workerLocalStorage(new vec_u[nVectors]);
 
-	foreach (k, ref post; parallel(posts, 1))
+	foreach (k, post; parallel(posts, 1))
 	{
 		PostsWithSharedTags[TopN] top5;
-		Post[TopN] topPosts;
 
 		auto taggedPostsVec = taggedPostsVecThreadPool.get;
 		auto taggedPostsCount = cast(ubyte[]) taggedPostsVec;
@@ -72,39 +67,33 @@ void main()
 
 		taggedPostsCount[k] = 0;
 
-		top5[] = PostsWithSharedTags(0, 0);
-
 		vec_u minTags = 0;
-		ulong pv = 0;
-		vec_u eqMask = cast(ubyte)-1;
-		while (pv < nVectors)
+
+		foreach (vecIdx, vecElem; taggedPostsVec)
 		{
-			while (pv < nVectors && ((taggedPostsVec[pv] <= minTags) is eqMask))
-				pv++;
-			if (pv < nVectors)
+			if ((vecElem > minTags) is vec_u.init)
+				continue;
+
+			foreach (l, el; vecElem)
 			{
-				vec_u counts = taggedPostsVec[pv];
-
-				foreach (l; 0 .. vSize)
+				if (el > minTags[0])
 				{
-					if (counts[l] > minTags[0])
+					int upperBound = TopN - 2;
+
+					while (upperBound >= 0 && el > top5[upperBound].sharedTags)
 					{
-						int upperBound = TopN - 2;
-
-						while (upperBound >= 0 && counts[l] > top5[upperBound].sharedTags)
-						{
-							top5[upperBound + 1] = top5[upperBound];
-							upperBound--;
-						}
-
-						top5[upperBound + 1] = PostsWithSharedTags(pv * vSize + l, counts[l]);
-
-						minTags = cast(vec_u) top5[TopN - 1].sharedTags;
+						top5[upperBound + 1] = top5[upperBound];
+						upperBound--;
 					}
+
+					top5[upperBound + 1] = PostsWithSharedTags(vecIdx * vSize + l, el);
+
+					minTags[] = top5[TopN - 1].sharedTags;
 				}
 			}
-			pv++;
 		}
+
+		Post[TopN] topPosts;
 
 		foreach (i, t; top5)
 			topPosts[i] = posts[t.post];
