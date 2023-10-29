59e067e2607e729a69664168f39a71659d106ef7
Tortar <68152031+Tortar@users.noreply.github.com>
2023-10-29 15:01:22 +0100

    Restore version with StaticArrays for concurrent Julia

--- julia_con/RelatedCon/src/RelatedCon.jl
+++ julia_con/RelatedCon/src/RelatedCon.jl
@@ -1,63 +1,69 @@
 module RelatedCon
 
-using JSON3
-using StructTypes
-using Dates
-using PrecompileTools
-using ChunkSplitters
+using JSON3, StructTypes, Dates, StaticArrays, ChunkSplitters
 using Base.Threads: @threads, nthreads
 
-const topn = 5
-
 export main
+
+const topn = 5
 
 struct PostData
     _id::String
     title::String
-    tags::Vector{Symbol}
+    tags::Vector{String}
 end
 
 struct RelatedPost
     _id::String
-    tags::Vector{Symbol}
-    related::NTuple{topn, PostData}
+    tags::Vector{String}
+    related::SVector{topn, PostData}
 end
 
 StructTypes.StructType(::Type{PostData}) = StructTypes.Struct()
 
-function maxindex!(xs::Vector, maxs)
-    # each element is a pair idx => val
-    maxs .= (1 => 0)
-    top = maxs[1][2]
+function fastmaxindex!(xs::Vector, topn, maxn, maxv)
+    maxn .= 1
+    maxv .= 0
+    top = maxv[1]
     for (i, x) in enumerate(xs)
         if x > top
-            maxs[1] = (i => x)
+            maxn[1], maxv[1] = i, x
             for j in 2:topn
-                if maxs[j-1][2] > maxs[j][2]
-                    maxs[j-1], maxs[j] = maxs[j], maxs[j-1]
+                if maxv[j-1] > maxv[j]
+                    maxv[j-1], maxv[j] = maxv[j], maxv[j-1]
+                    maxn[j-1], maxn[j] = maxn[j], maxn[j-1]
                 end
             end
-            top = maxs[1][2]
+            top = maxv[1]
         end
     end
-    maxs
+    reverse!(maxn)
+    return
 end
 
-function related!(posts, tagmap, relatedposts)
+function related(posts)
+    Ts = (UInt8, UInt16, UInt32, UInt64)
+    i = findfirst(T -> length(posts) < typemax(T), Ts)
+    return related(Ts[i], posts)
+end
+function related(::Type{T}, posts) where {T}
     # key is every possible "tag" used in all posts
     # value is indicies of all "post"s that used this tag
-
+    tagmap = Dict{String,Vector{T}}()
     for (idx, post) in enumerate(posts)
         for tag in post.tags
-            tags = get!(() -> sizehint!(Int32[], 1_000), tagmap, tag)
+            tags = get!(() -> T[], tagmap, tag)
             push!(tags, idx)
         end
     end
 
-    @threads for (postsrange, _) in chunks(posts, 2*nthreads())
-        
-        maxs = Vector{Pair{Int, Int32}}(undef, topn)
-        taggedpostcount = Vector{Int32}(undef, length(posts))
+    relatedposts = Vector{RelatedPost}(undef, length(posts))
+
+    @threads for (postsrange, _) in chunks(posts, nthreads())
+        topn = 5
+        maxn = MVector{topn,T}(undef)
+        maxv = MVector{topn,T}(undef)
+        taggedpostcount = Vector{T}(undef, length(posts))
 
         for i in postsrange
             post = posts[i]
@@ -68,15 +74,17 @@ function related!(posts, tagmap, relatedposts)
             # give all related post +1 in `taggedpostcount` shadow vector
             for tag in post.tags
                 for idx in tagmap[tag]
-                    taggedpostcount[idx] += one(Int32)
+                    taggedpostcount[idx] += one(T)
                 end
             end
+
             # don't self count
             taggedpostcount[i] = 0
-            
-            maxindex!(taggedpostcount, maxs)
 
-            relatedposts[i] = RelatedPost(post._id, post.tags, ntuple(i -> posts[maxs[topn+1-i][1]], topn))
+            fastmaxindex!(taggedpostcount, topn, maxn, maxv)
+
+            relatedpost = RelatedPost(post._id, post.tags, SVector{topn}(@view posts[maxn]))
+            relatedposts[i] = relatedpost
         end
     end
 
@@ -86,24 +94,16 @@ end
 function main()
     json_string = read(@__DIR__()*"/../../../posts.json", String)
     posts = JSON3.read(json_string, Vector{PostData})
-    
-    start = now()       
+    fake_posts = fill(posts[1], length(posts))
+    related(fake_posts) #warmup
 
-    tagmap=Dict{Symbol, Vector{Int32}}()
-    relatedposts=Vector{RelatedPost}(undef, length(posts))
-
-    all_related_posts = related!(posts, tagmap, relatedposts)
+    start = now()
+    all_related_posts = related(posts)
     println("Processing time (w/o IO): $(now() - start)")
 
-    open(@__DIR__()*"/../../../related_posts_julia_con.json", "w") do f
+    open(@__DIR__()*"/../../../related_posts_julia.json", "w") do f
         JSON3.write(f, all_related_posts)
     end
-end
-
-
-@compile_workload begin
-    print("Precompiling main workload: ")
-    main()
 end
 
 end # module RelatedCon
