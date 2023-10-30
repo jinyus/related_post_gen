533046c7b41671037acf811b5b65603323ce16d7
Tortar <68152031+Tortar@users.noreply.github.com>
2023-10-30 16:22:35 +0100

    Stabilize performance of Julia implementation

--- julia/Related/src/Related.jl
+++ julia/Related/src/Related.jl
@@ -20,13 +20,14 @@ end
 
 StructTypes.StructType(::Type{PostData}) = StructTypes.Struct()
 
-function fastmaxindex!(xs::Vector{T}, topn, maxn, maxv) where {T}
-    maxn .= one(T)
-    maxv .= zero(T)
+function fastmaxindex!(xs::Vector, topn, maxn, maxv)
+    maxn .= 1
+    maxv .= 0
     top = maxv[1]
     for (i, x) in enumerate(xs)
         if x > top
-            maxn[1], maxv[1] = i, x
+            maxv[1] = x
+            maxn[1] = i
             for j in 2:topn
                 if maxv[j-1] > maxv[j]
                     maxv[j-1], maxv[j] = maxv[j], maxv[j-1]
@@ -37,7 +38,7 @@ function fastmaxindex!(xs::Vector{T}, topn, maxn, maxv) where {T}
         end
     end
     reverse!(maxn)
-    return 
+    return maxn
 end
 
 function related(posts)
@@ -46,6 +47,7 @@ function related(posts)
     # key is every possible "tag" used in all posts
     # value is indicies of all "post"s that used this tag
     tagmap = Dict{String,Vector{T}}()
+    sizehint!(tagmap, 100)
     for (idx, post) in enumerate(posts)
         for tag in post.tags
             tags = get!(() -> T[], tagmap, tag)
@@ -60,7 +62,7 @@ function related(posts)
     maxv = MVector{topn,T}(undef)
 
     for (i, post) in enumerate(posts)
-        taggedpostcount .= zero(T)
+        taggedpostcount .= 0
         # for each post (`i`-th)
         # and every tag used in the `i`-th post
         # give all related post +1 in `taggedpostcount` shadow vector
@@ -71,7 +73,7 @@ function related(posts)
         end
 
         # don't self count
-        taggedpostcount[i] = zero(T)
+        taggedpostcount[i] = 0
 
         fastmaxindex!(taggedpostcount, topn, maxn, maxv)
 
