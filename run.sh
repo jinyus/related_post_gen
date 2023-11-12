0a5881faa6a69f5ed0ef2c1ed5251b07710a3765
jinyus <starboymc4@gmail.com>
2023-11-12 11:57:44 -0500

    Commented out slow Ruby script in run.sh

--- run.sh
+++ run.sh
@@ -1105,7 +1105,7 @@ elif [ "$first_arg" = "all" ]; then
         run_lua || echo -e "\n" &&
         run_ocaml || echo -e "\n" &&
         run_erlang || echo -e "\n" &&
-        run_ruby || echo -e "\n" &&
+        # run_ruby || echo -e "\n" && # too slow
         # run_dascript || echo -e "\n" && #not installed in docker
         run_lobster || echo -e "\n" &&
         echo -e "Finished running all\n"
