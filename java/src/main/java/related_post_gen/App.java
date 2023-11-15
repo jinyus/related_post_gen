f38a34d310f1e40419b52ebc614ec7e0c3878b36
jinyus <starboymc4@gmail.com>
2023-11-15 01:22:23 -0500

    run gc after json parsing

--- java/src/main/java/related_post_gen/App.java
+++ java/src/main/java/related_post_gen/App.java
@@ -18,112 +18,109 @@ import java.util.Objects;
 import java.util.concurrent.TimeUnit;
 
 public class App {
-    @CompiledJson
-    record Post(@JsonAttribute(name = "_id") String id,
-                String title,
-                List<String> tags) {
-    }
+	@CompiledJson
+	record Post(@JsonAttribute(name = "_id") String id, String title, List<String> tags) {
+	}
 
-    record PostWithSharedTags(int post, int sharedTags) {
-    }
+	record PostWithSharedTags(int post, int sharedTags) {
+	}
 
-    @CompiledJson
-    record RelatedPosts(@JsonAttribute(name = "_id") String id,
-                        List<String> tags,
-                        Post[] related) {
-    }
+	@CompiledJson
+	record RelatedPosts(@JsonAttribute(name = "_id") String id, List<String> tags, Post[] related) {
+	}
 
-    public static void main(String[] args) throws IOException {
-        DslJson<Object> dslJson = new DslJson<>();
+	public static void main(String[] args) throws IOException {
+		DslJson<Object> dslJson = new DslJson<>();
 
-        Post[] posts;
-        try (InputStream in = Files.newInputStream(Paths.get("../posts.json"))) {
-            posts = Objects.requireNonNull(dslJson.deserialize(Post[].class, in));
-        }
+		Post[] posts;
+		try (InputStream in = Files.newInputStream(Paths.get("../posts.json"))) {
+			posts = Objects.requireNonNull(dslJson.deserialize(Post[].class, in));
+		}
 
-        // warmup
-        getAllRelatedPosts(posts);
+		// warmup
+		getAllRelatedPosts(posts);
 
-        long start = System.nanoTime();
+		System.gc();
 
-        var allRelatedPosts = getAllRelatedPosts(posts);
+		long start = System.nanoTime();
 
-        long end = System.nanoTime();
+		var allRelatedPosts = getAllRelatedPosts(posts);
 
-        System.out.println("Processing time (w/o IO): " + TimeUnit.NANOSECONDS.toMillis(end - start) + " ms");
+		long end = System.nanoTime();
 
-        try (OutputStream out = Files.newOutputStream(Paths.get("../related_posts_java.json"))) {
-            dslJson.serialize(allRelatedPosts, out);
-        }
-    }
+		System.out.println("Processing time (w/o IO): " + TimeUnit.NANOSECONDS.toMillis(end - start) + " ms");
 
-    public static RelatedPosts[] getAllRelatedPosts(Post[] posts) throws IOException {
-        
+		try (OutputStream out = Files.newOutputStream(Paths.get("../related_posts_java.json"))) {
+			dslJson.serialize(allRelatedPosts, out);
+		}
+	}
 
+	public static RelatedPosts[] getAllRelatedPosts(Post[] posts) throws IOException {
 
-        Map<String, IntArrayList> tagMap = HashMap.newHashMap(100);
-        for (int i = 0; i < posts.length; i++) {
-            for (String tag : posts[i].tags()) {
-                tagMap.computeIfAbsent(tag, k -> new IntArrayList()).add(i);
-            }
-        }
+		Map<String, IntArrayList> tagMap = HashMap.newHashMap(100);
+		for (int i = 0; i < posts.length; i++) {
+			for (String tag : posts[i].tags()) {
+				tagMap.computeIfAbsent(tag, k -> new IntArrayList()).add(i);
+			}
+		}
 
-        var allRelatedPosts = new RelatedPosts[posts.length];
-        var taggedPostCount = new int[posts.length];
+		var allRelatedPosts = new RelatedPosts[posts.length];
+		var taggedPostCount = new int[posts.length];
 
-        for (int i = 0; i < posts.length; i++) {
-            allRelatedPosts[i] = getRelatedPosts(posts, i, taggedPostCount, tagMap);
-        }
+		for (int i = 0; i < posts.length; i++) {
+			allRelatedPosts[i] = getRelatedPosts(posts, i, taggedPostCount, tagMap);
+		}
 
-        return allRelatedPosts;
-    }
+		return allRelatedPosts;
+	}
 
-    private static RelatedPosts getRelatedPosts(Post[] posts, int i, int[] taggedPostCount, Map<String, IntArrayList> tagMap) {
-        Post post = posts[i];
+	private static RelatedPosts getRelatedPosts(Post[] posts, int i, int[] taggedPostCount,
+			Map<String, IntArrayList> tagMap) {
+		Post post = posts[i];
 
-        // optimized to a memset
-        Arrays.fill(taggedPostCount, 0);
+		// optimized to a memset
+		Arrays.fill(taggedPostCount, 0);
 
-        for (String tag : post.tags()) {
-            tagMap.get(tag).forEach(otherPostIdx -> taggedPostCount[otherPostIdx]++);
-        }
+		for (String tag : post.tags()) {
+			tagMap.get(tag).forEach(otherPostIdx -> taggedPostCount[otherPostIdx]++);
+		}
 
-        taggedPostCount[i] = 0; // Don't count self
+		taggedPostCount[i] = 0; // Don't count self
 
-        var top5 = new PostWithSharedTags[5];
-        for (int j = 0; j < top5.length; j++) {
-            top5[j] = new PostWithSharedTags(0, 0);
-        }
-        int minTags = 0; // Updated initialization
+		var top5 = new PostWithSharedTags[5];
+		for (int j = 0; j < top5.length; j++) {
+			top5[j] = new PostWithSharedTags(0, 0);
+		}
+		int minTags = 0; // Updated initialization
 
-        for (int j = 0; j < taggedPostCount.length; j++) {
-            int count = taggedPostCount[j];
+		for (int j = 0; j < taggedPostCount.length; j++) {
+			int count = taggedPostCount[j];
 
-            if (count > minTags) {
-                // Find the position to insert
-                int pos = 4;
-                while (pos >= 0 && top5[pos].sharedTags < count) {
-                    pos--;
-                }
-                pos++;
+			if (count > minTags) {
+				// Find the position to insert
+				int pos = 4;
+				while (pos >= 0 && top5[pos].sharedTags < count) {
+					pos--;
+				}
+				pos++;
 
-                // Shift and insert
-                if (pos < 4) {
-                    System.arraycopy(top5, pos, top5, pos + 1, 4 - pos);
-                }
+				// Shift and insert
+				if (pos < 4) {
+					System.arraycopy(top5, pos, top5, pos + 1, 4 - pos);
+				}
 
-                top5[pos] = new PostWithSharedTags(j, count);
-                minTags = top5[4].sharedTags;
-            }
-        }
+				top5[pos] = new PostWithSharedTags(j, count);
+				minTags = top5[4].sharedTags;
+			}
+		}
 
-        // Convert indexes back to Post pointers
-        Post[] topPosts = new Post[5];
-        int index = 0;
-        for (PostWithSharedTags t : top5) {
-            topPosts[index++] = posts[t.post];
-        }
+		// Convert indexes back to Post pointers
+		Post[] topPosts = new Post[5];
+		int index = 0;
+		for (PostWithSharedTags t : top5) {
+			topPosts[index++] = posts[t.post];
+		}
 
-        return new RelatedPosts(post.id(), post.tags(), topPosts);
-    }
+		return new RelatedPosts(post.id(), post.tags(), topPosts);
+	}
 }
--- java/src/main/java/related_post_gen/AppParallel.java
+++ java/src/main/java/related_post_gen/AppParallel.java
@@ -17,103 +17,107 @@ import java.util.concurrent.ForkJoinPool;
 import java.util.concurrent.TimeUnit;
 
 public class AppParallel {
-    public static void main(String[] args) throws IOException, InterruptedException {
-        parallel("../posts.json", "../related_posts_java_con.json");
-    }
+	public static void main(String[] args) throws IOException, InterruptedException {
+		parallel("../posts.json", "../related_posts_java_con.json");
+	}
 
-    public static void parallel(String inputFile, String outputFile) throws IOException, InterruptedException {
-        ForkJoinPool threadPool = ForkJoinPool.commonPool();
-        int concurrency = threadPool.getParallelism();
-        DslJson<Object> dslJson = new DslJson<>();
+	public static void parallel(String inputFile, String outputFile) throws IOException, InterruptedException {
+		DslJson<Object> dslJson = new DslJson<>();
 
-        App.Post[] posts;
-        try (InputStream in = Files.newInputStream(Paths.get(inputFile))) {
-            posts = Objects.requireNonNull(dslJson.deserialize(App.Post[].class, in));
-        }
+		App.Post[] posts;
+		try (InputStream in = Files.newInputStream(Paths.get(inputFile))) {
+			posts = Objects.requireNonNull(dslJson.deserialize(App.Post[].class, in));
+		}
 
-        long start = System.nanoTime();
+		System.gc();
 
-        final int postsLength = posts.length;
+		long start = System.nanoTime();
 
-        Map<String, IntArrayList> tagMap = HashMap.newHashMap(100);
-        for (int i = 0; i < postsLength; i++) {
-            for (String tag : posts[i].tags()) {
-                tagMap.computeIfAbsent(tag, k -> new IntArrayList()).add(i);
-            }
-        }
+		ForkJoinPool threadPool = ForkJoinPool.commonPool();
+		int concurrency = threadPool.getParallelism();
 
-        var allRelatedPosts = new App.RelatedPosts[postsLength];
+		final int postsLength = posts.length;
 
-        int[] allTaggedPostCount = new int[concurrency * postsLength];
-        CountDownLatch waitGroup = new CountDownLatch(concurrency);
-        for (int w = 0; w < concurrency; w++) {
-            final int workerId = w;
-            threadPool.execute(() -> {
-                for (int i = workerId; i < postsLength; i += concurrency) {
-                    allRelatedPosts[i] = computeRelatedPost(i, posts, tagMap, allTaggedPostCount, workerId * postsLength);
-                }
-                waitGroup.countDown();
-            });
-        }
+		Map<String, IntArrayList> tagMap = HashMap.newHashMap(100);
+		for (int i = 0; i < postsLength; i++) {
+			for (String tag : posts[i].tags()) {
+				tagMap.computeIfAbsent(tag, k -> new IntArrayList()).add(i);
+			}
+		}
 
-        waitGroup.await();
+		var allRelatedPosts = new App.RelatedPosts[postsLength];
 
-        long end = System.nanoTime();
+		int[] allTaggedPostCount = new int[concurrency * postsLength];
+		CountDownLatch waitGroup = new CountDownLatch(concurrency);
+		for (int w = 0; w < concurrency; w++) {
+			final int workerId = w;
+			threadPool.execute(() -> {
+				for (int i = workerId; i < postsLength; i += concurrency) {
+					allRelatedPosts[i] = computeRelatedPost(i, posts, tagMap, allTaggedPostCount,
+							workerId * postsLength);
+				}
+				waitGroup.countDown();
+			});
+		}
 
-        System.out.println("Processing time (w/o IO): " + TimeUnit.NANOSECONDS.toMillis(end - start) + " ms");
+		waitGroup.await();
 
-        try (OutputStream out = Files.newOutputStream(Paths.get(outputFile))) {
-            dslJson.serialize(allRelatedPosts, out);
-        }
-    }
+		long end = System.nanoTime();
 
-    private static App.RelatedPosts computeRelatedPost(int i, App.Post[] posts, Map<String, IntArrayList> tagMap,
-                                                       int[] taggedPostCount, int startPostCountIdx) {
-        App.Post post = posts[i];
+		System.out.println("Processing time (w/o IO): " + TimeUnit.NANOSECONDS.toMillis(end - start) + " ms");
 
-        // optimized to a memset
-        Arrays.fill(taggedPostCount, startPostCountIdx, startPostCountIdx + posts.length, 0);
+		try (OutputStream out = Files.newOutputStream(Paths.get(outputFile))) {
+			dslJson.serialize(allRelatedPosts, out);
+		}
+	}
 
-        for (String tag : post.tags()) {
-            tagMap.get(tag).forEach(otherPostIdx -> taggedPostCount[startPostCountIdx + otherPostIdx]++);
-        }
+	private static App.RelatedPosts computeRelatedPost(int i, App.Post[] posts, Map<String, IntArrayList> tagMap,
+			int[] taggedPostCount, int startPostCountIdx) {
+		App.Post post = posts[i];
 
-        taggedPostCount[startPostCountIdx + i] = 0; // Don't count self
+		// optimized to a memset
+		Arrays.fill(taggedPostCount, startPostCountIdx, startPostCountIdx + posts.length, 0);
 
-        var top5 = new App.PostWithSharedTags[5];
-        for (int j = 0; j < top5.length; j++) {
-            top5[j] = new App.PostWithSharedTags(0, 0);
-        }
-        int minTags = 0; // Updated initialization
+		for (String tag : post.tags()) {
+			tagMap.get(tag).forEach(otherPostIdx -> taggedPostCount[startPostCountIdx + otherPostIdx]++);
+		}
 
-        for (int j = 0; j < posts.length; j++) {
-            int count = taggedPostCount[startPostCountIdx + j];
+		taggedPostCount[startPostCountIdx + i] = 0; // Don't count self
 
-            if (count > minTags) {
-                // Find the position to insert
-                int pos = 4;
-                while (pos >= 0 && top5[pos].sharedTags() < count) {
-                    pos--;
-                }
-                pos++;
+		var top5 = new App.PostWithSharedTags[5];
+		for (int j = 0; j < top5.length; j++) {
+			top5[j] = new App.PostWithSharedTags(0, 0);
+		}
+		int minTags = 0; // Updated initialization
 
-                // Shift and insert
-                if (pos < 4) {
-                    System.arraycopy(top5, pos, top5, pos + 1, 4 - pos);
-                }
+		for (int j = 0; j < posts.length; j++) {
+			int count = taggedPostCount[startPostCountIdx + j];
 
-                top5[pos] = new App.PostWithSharedTags(j, count);
-                minTags = top5[4].sharedTags();
-            }
-        }
+			if (count > minTags) {
+				// Find the position to insert
+				int pos = 4;
+				while (pos >= 0 && top5[pos].sharedTags() < count) {
+					pos--;
+				}
+				pos++;
 
-        // Convert indexes back to Post pointers
-        App.Post[] topPosts = new App.Post[5];
-        int index = 0;
-        for (App.PostWithSharedTags t : top5) {
-            topPosts[index++] = posts[t.post()];
-        }
+				// Shift and insert
+				if (pos < 4) {
+					System.arraycopy(top5, pos, top5, pos + 1, 4 - pos);
+				}
 
-        return new App.RelatedPosts(post.id(), post.tags(), topPosts);
-    }
+				top5[pos] = new App.PostWithSharedTags(j, count);
+				minTags = top5[4].sharedTags();
+			}
+		}
+
+		// Convert indexes back to Post pointers
+		App.Post[] topPosts = new App.Post[5];
+		int index = 0;
+		for (App.PostWithSharedTags t : top5) {
+			topPosts[index++] = posts[t.post()];
+		}
+
+		return new App.RelatedPosts(post.id(), post.tags(), topPosts);
+	}
 }
