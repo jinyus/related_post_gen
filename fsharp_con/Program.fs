2c03a0c5d300e3645c5c4db6b28b1c20a4b1b1b2
Denis <onlydelneg@gmail.com>
2023-11-15 19:18:41 +0400

    Update concurrent impl with development from sync impls

--- fsharp_con/Program.fs
+++ fsharp_con/Program.fs
@@ -1,4 +1,5 @@
 ﻿open System
+open System.Collections.Frozen
 open System.IO
 open FSharp.NativeInterop
 open System.Collections.Generic
@@ -10,15 +11,22 @@ let inline stackalloc<'a when 'a: unmanaged> (length: int) : Span<'a> =
     let p = NativePtr.stackalloc<'a> length |> NativePtr.toVoidPtr
     Span<'a>(p, length)
 
+let inline incv (x : byref<_>) = x <- x + LanguagePrimitives.GenericOne
+
+[<Struct>]
 type Post =
     { _id: string
       title: string
       tags: string[] }
 
+[<Struct>]
 type RelatedPosts =
     { _id: string
       tags: string[]
       related: Post[] }
+
+[<Struct>]
+type TopNItem =  { count: byte; postId: int }
 
 [<Literal>]
 let srcDir = __SOURCE_DIRECTORY__
@@ -26,71 +34,74 @@ let srcDir = __SOURCE_DIRECTORY__
 [<Literal>]
 let topN = 5
 
+let getRelatedPosts (tagMap: FrozenDictionary<string, int[]>) (posts: ResizeArray<Post>) (i: int)  (post: Post) =
+    let taggedPostCount = stackalloc posts.Count
+    taggedPostCount.Fill(0uy)
+    for tag in post.tags do
+        for otherPostIdx in tagMap[tag] do
+            incv &taggedPostCount[otherPostIdx]
+    
+    taggedPostCount[i] <- 0uy // ignore self
+    let top5 = Array.zeroCreate<TopNItem> topN
+    let mutable minTags: byte = 0uy
+    let taggedPostLength = uint taggedPostCount.Length
+    
+    //  custom priority queue to find top N
+    let mutable p: int = 0
+    while (uint p < taggedPostLength) do
+        while ((uint p < taggedPostLength) && (taggedPostCount[p] <= minTags)) do
+            p <- p + 1
+        if (uint p < taggedPostLength) then
+            let count: byte = taggedPostCount[p]
+            let mutable upperBound: int = topN - 2
+            while upperBound >= 0 && count > top5[upperBound].count do
+                top5[upperBound + 1] <- top5[upperBound]
+                upperBound <- upperBound - 1
 
-let getAllRelated (posts: Post[]) =
+            top5[upperBound + 1] <- { count = count; postId = p }
+            minTags <- top5[topN - 1].count
+        
+        p <- p + 1
+        
+    let topPosts = Array.zeroCreate<Post>(topN)
+    
+    let mutable j = 0
+    while j < 5 do
+        topPosts[j] <- posts[top5[j].postId]
+        j <- j + 1
+        
+    
+    { _id = post._id
+      tags = post.tags
+      related = topPosts }
+        
+
+let getAllRelated (posts: ResizeArray<Post>) =
+    let postsCount = posts.Count
+    
     // Start work
-    let tagPostsTmp = Dictionary<string, Stack<int>>()
+    let tagPostsTmp = Dictionary<string, LinkedList<int>>(100)
 
-    posts
-    |> Array.iteri (fun postId post ->
-
-        for tag in post.tags do
-
+    for i in 0 .. postsCount - 1 do
+        for tag in posts[i].tags do
             match tagPostsTmp.TryGetValue tag with
-            | true, s -> s.Push postId
+            | true, s -> s.AddLast i |> ignore
             | false, _ ->
-                let newStack = Stack()
-                newStack.Push postId
-                tagPostsTmp[tag] <- newStack)
-
-    // convert from Dict<_,Stack<int>> to Dict<_,int[]> for faster access
-    let tagPosts = Dictionary(tagPostsTmp.Count)
-
-    for kv in tagPostsTmp do
-        tagPosts[kv.Key] <- kv.Value.ToArray()
-
-
-    let getRelated (idx: int) (post: Post) =
-        let taggedPostCount = stackalloc posts.Length
-
-        for tagId in post.tags do
-            for relatedPostId in tagPosts[tagId] do
-                let mutable relatedPostTagCount = &taggedPostCount[relatedPostId]
-                relatedPostTagCount <- relatedPostTagCount + 1uy
-
-        taggedPostCount[idx] <- 0uy // ignore self
-
-        let top5 = Array.zeroCreate<struct {| count: byte; postId: int |}> topN
-        let mutable minTags = 0uy
-
-        for i in 0 .. taggedPostCount.Length - 1 do
-            let count = taggedPostCount[i]
-
-            if count > minTags then
-                // Find upper bound: pos at which count is larger than current one.
-                let mutable pos = topN - 2
-
-                while pos >= 0 && count > top5[pos].count do
-                    top5[pos + 1] <- top5[pos]
-                    pos <- pos - 1
-
-                top5[pos + 1] <- {| count = count; postId = i |}
-                minTags <- top5[topN - 1].count
-
-
-        { _id = post._id
-          tags = post.tags
-          related = top5 |> Array.map (fun top -> posts[top.postId]) }
-
-
-    posts |> Array.Parallel.mapi getRelated
-
+                let stack = LinkedList()
+                stack.AddLast i |> ignore
+                tagPostsTmp[tag] <- stack
+                
+    // convert from Dict<_,LinkedList<int>> to Dict<_,int[]> for faster access
+    let tagMap = FrozenDictionary.ToFrozenDictionary(tagPostsTmp, (fun s -> s.Key), fun s -> System.Linq.Enumerable.ToArray(s.Value))
+    let appliedRelated = getRelatedPosts tagMap posts
+    
+    Array.Parallel.mapi appliedRelated (posts.ToArray())
 
 
 
 [<EntryPoint>]
 let main args =
-    let posts = Json.deserialize<Post[]> (File.ReadAllText $"{srcDir}/../posts.json")
+    let posts = Json.deserialize<Post[]> (File.ReadAllText $"{srcDir}/../posts.json") |> ResizeArray
 
     // Warmup
     getAllRelated posts |> ignore
@@ -102,7 +113,7 @@ let main args =
     let allRelatedPosts = getAllRelated posts
 
     stopwatch.Stop()
-    Console.WriteLine($"Processing time (w/o IO): %d{stopwatch.ElapsedMilliseconds}ms")
+    Console.WriteLine($"Processing time (w/o IO): {stopwatch.Elapsed.TotalMilliseconds}ms")
 
     let json = Json.serialize allRelatedPosts
 
