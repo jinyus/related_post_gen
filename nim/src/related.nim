a4ac3a3628ee5b7fbc225d07a94bb446f8e5ad58
Michael Bradley, Jr. <michaelsbradleyjr@gmail.com>
2023-11-19 16:15:32 -0600

    [Nim] `collect(newSeqOfCap(posts.len)):` does not yield perf boost

--- nim/src/related.nim
+++ nim/src/related.nim
@@ -4,7 +4,6 @@ import pkg/[decimal, jsony, xxhash]
 # questions re: performance in Azure VM/Docker
 # --------------------------------------------
 # ? is returning seq from findTopN faster than returning array
-# ? is `collect(newSeqOfCap(posts.len)):` faster than `collect:`
 # ? how to correctly round DecimalType to 2 places
 
 const N: Positive = 5
@@ -86,7 +85,6 @@ func findTopN(
 
 func process(posts: seq[Post]): seq[RelatedPosts] =
   let tagMap = posts.genTagMap
-  # collect(newSeqOfCap(posts.len)):
   collect:
     for i, post in posts:
       RelatedPosts(
