da8296e452d631450521671cda855aa5031a615e
Folke Behrens <folke@gmail.com>
2023-11-20 19:58:32 +0100

    [Rust con] Sync with single-threaded version

--- rust_con/src/main.rs
+++ rust_con/src/main.rs
@@ -1,75 +1,94 @@
-use std::cell::RefCell;
-use std::hint;
-use std::time::Instant;
-
 use rayon::prelude::*;
-
-mod least;
-mod types;
-mod utils;
-
-use types::{Post, RelatedPosts};
+use rustc_data_structures::fx::FxHashMap;
+use serde::{Deserialize, Serialize};
+use std::time::Instant;
 
 #[global_allocator]
 static GLOBAL: mimalloc::MiMalloc = mimalloc::MiMalloc;
 
 const NUM_TOP_ITEMS: usize = 5;
 
-fn main() {
-    // setup the runtime
-    let num_cpus = num_cpus::get_physical(); // does IO to get num_cpus
-    rayon::ThreadPoolBuilder::new()
-        .num_threads(num_cpus)
-        .build_global()
-        .unwrap();
+#[derive(Serialize, Deserialize)]
+#[repr(align(64))]
+struct Post<'a> {
+    #[serde(rename = "_id")]
+    id: &'a str,
+    title: &'a str,
+    tags: Vec<&'a str>,
+}
 
-    // setup the input
+#[derive(Serialize)]
+#[repr(align(64))]
+struct RelatedPosts<'a> {
+    #[serde(rename = "_id")]
+    id: &'a str,
+    tags: &'a [&'a str],
+    related: [&'a Post<'a>; NUM_TOP_ITEMS],
+}
+
+fn main() {
+    rayon::ThreadPoolBuilder::new().build_global().unwrap();
+
     let json_str = std::fs::read_to_string("../posts.json").unwrap();
     let posts: Vec<Post> = serde_json::from_str(&json_str).unwrap();
 
-    // time the algorithm
-    let start = hint::black_box(Instant::now());
-    let related_posts = get_related(&posts);
-    let end = hint::black_box(Instant::now());
+    let start = Instant::now();
 
-    println!("Processing time (w/o IO): {:?}", end.duration_since(start));
-
-    let json_str = serde_json::to_string(&related_posts).unwrap();
-    std::fs::write("../related_posts_rust_con.json", json_str).unwrap();
-}
-
-fn get_related<'a>(posts: &'a [Post]) -> Vec<RelatedPosts<'a>> {
-    thread_local! {
-        static POST_COUNT: RefCell<Vec<u8>> = panic!("!");
-    }
-    rayon::broadcast(|_| {
-        POST_COUNT.set(vec![0; posts.len()]);
+    let mut post_tags_map: FxHashMap<&str, Vec<u32>> = Default::default();
+    posts.iter().enumerate().for_each(|(post_idx, post)| {
+        for tag in &post.tags {
+            post_tags_map.entry(tag).or_default().push(post_idx as u32);
+        }
     });
 
-    let post_tags_map = utils::get_post_tags_map(posts);
-
-    posts
+    let related_posts: Vec<RelatedPosts<'_>> = posts
         .par_iter()
         .enumerate()
-        .map(|(idx, post)| {
-            POST_COUNT.with_borrow_mut(|tagged_post_count| {
+        .map_with(
+            vec![0u8; posts.len()],
+            |tagged_post_count, (post_idx, post)| {
                 for tag in &post.tags {
                     if let Some(tag_posts) = post_tags_map.get(tag) {
-                        for &other_post_idx in tag_posts {
-                            tagged_post_count[other_post_idx as usize] += 1;
+                        for other_post_idx in tag_posts {
+                            tagged_post_count[*other_post_idx as usize] += 1;
                         }
                     }
                 }
-                tagged_post_count[idx] = 0;
+                tagged_post_count[post_idx] = 0; // don't recommend the same post
 
-                let rp = RelatedPosts {
+                let mut top_k = [(0u8, 0u32); NUM_TOP_ITEMS];
+                let mut min_tags = 0;
+                for (j, &count) in tagged_post_count.iter().enumerate() {
+                    if count > min_tags {
+                        let pos = top_k
+                            .iter()
+                            .rev()
+                            .position(|t| t.0 > count)
+                            .map_or(0, |p| NUM_TOP_ITEMS - p);
+
+                        top_k.copy_within(pos..NUM_TOP_ITEMS - 1, pos + 1);
+                        top_k[pos] = (count, j as u32);
+
+                        min_tags = top_k[NUM_TOP_ITEMS - 1].0;
+                    }
+                }
+
+                tagged_post_count.fill(0);
+
+                // Convert indexes back to Post pointers
+                let related = top_k.map(|(_, j)| &posts[j as usize]);
+                RelatedPosts {
                     id: post.id,
                     tags: &post.tags,
-                    related: utils::get_related(NUM_TOP_ITEMS, tagged_post_count, posts),
-                };
-                tagged_post_count.fill(0);
-                rp
-            })
-        })
-        .collect()
+                    related,
+                }
+            },
+        )
+        .collect();
+
+    let dur = start.elapsed();
+    println!("Processing time (w/o IO): {dur:?}");
+
+    let json_str = serde_json::to_string(&related_posts).unwrap();
+    std::fs::write("../related_posts_rust_con.json", json_str).unwrap();
 }
