907e9c3352355724c0e6c743cb6658ec877370cb
jinyus <starboymc4@gmail.com>
2023-11-20 20:43:37 -0500

    use scala3 syntax

--- scala_native/src/main/scala/Main.scala
+++ scala_native/src/main/scala/Main.scala
@@ -12,81 +12,80 @@ object RelatedPost {
   implicit val rw: ReadWriter[RelatedPost] = upickle.default.macroRW
 }
 
-object Main {
-  val TopN = 5
+val TopN = 5
 
-  def main(args: Array[String]): Unit =
-    val jsonContent = os.read(os.pwd / os.up / "posts.json")
+@main
+def main(): Unit =
+  val jsonContent = os.read(os.pwd / os.up / "posts.json")
 
-    val posts = upickle.default.read[Array[Post]](jsonContent)
+  val posts = upickle.default.read[Array[Post]](jsonContent)
 
-    val start = System.currentTimeMillis()
+  val start = System.currentTimeMillis()
 
-    val postsCount = posts.length
+  val postsCount = posts.length
 
-    var range = 0 until postsCount
+  var range = 0 until postsCount
 
-    val tagMapTemp = Map[String, Buffer[Int]]()
+  val tagMapTemp = Map[String, Buffer[Int]]()
 
-    for (i <- range) {
-      val post = posts(i)
-      for (tag <- post.tags) {
-        tagMapTemp.getOrElseUpdate(tag, Buffer()) += i
+  for (i <- range) {
+    val post = posts(i)
+    for (tag <- post.tags) {
+      tagMapTemp.getOrElseUpdate(tag, Buffer()) += i
+    }
+  }
+
+  // for faster access
+  val tagMap = tagMapTemp.map { case (tag, indexes) =>
+    tag -> indexes.toArray
+  }
+
+  val taggedPostCountTemp = Array.fill(postsCount)(0)
+  val topNTemp = Array.fill(TopN * 2)(0)
+
+  val allRelatedPosts = Array.tabulate(postsCount) { i =>
+    val post = posts(i)
+
+    // faster than allocating new array and mapInplace
+    val taggedPostCount = taggedPostCountTemp.clone()
+
+    for (tag <- post.tags) {
+      for (index <- tagMap(tag)) {
+        taggedPostCount(index) += 1
       }
     }
 
-    // for faster access
-    val tagMap = tagMapTemp.map { case (tag, indexes) =>
-      tag -> indexes.toArray
-    }
+    taggedPostCount(i) = 0
 
-    val taggedPostCountTemp = Array.fill(postsCount)(0)
-    val topNTemp = Array.fill(TopN * 2)(0)
+    val top5 = topNTemp.clone()
+    var minTags = 0
 
-    val allRelatedPosts = Array.tabulate(postsCount) { i =>
-      val post = posts(i)
+    for (j <- range) {
+      val count = taggedPostCount(j)
+      if (count > minTags) {
+        var upperBound = (TopN - 2) * 2
 
-      // faster than allocating new array and mapInplace
-      val taggedPostCount = taggedPostCountTemp.clone()
-
-      for (tag <- post.tags) {
-        for (index <- tagMap(tag)) {
-          taggedPostCount(index) += 1
+        while (upperBound >= 0 && count > top5(upperBound)) {
+          top5(upperBound + 2) = top5(upperBound)
+          top5(upperBound + 3) = top5(upperBound + 1)
+          upperBound -= 2
         }
+
+        val insertPos = upperBound + 2
+        top5(insertPos) = count
+        top5(insertPos + 1) = j
+
+        minTags = top5(TopN * 2 - 2)
       }
-
-      taggedPostCount(i) = 0
-
-      val top5 = topNTemp.clone()
-      var minTags = 0
-
-      for (j <- range) {
-        val count = taggedPostCount(j)
-        if (count > minTags) {
-          var upperBound = (TopN - 2) * 2
-
-          while (upperBound >= 0 && count > top5(upperBound)) {
-            top5(upperBound + 2) = top5(upperBound)
-            top5(upperBound + 3) = top5(upperBound + 1)
-            upperBound -= 2
-          }
-
-          val insertPos = upperBound + 2
-          top5(insertPos) = count
-          top5(insertPos + 1) = j
-
-          minTags = top5(TopN * 2 - 2)
-        }
-      }
-
-      val topPosts = Array.tabulate(TopN)(i => posts(top5(i * 2 + 1)))
-
-      RelatedPost(post._id, post.tags, topPosts)
     }
 
-    val end = System.currentTimeMillis()
-    println(s"Processing time (w/o IO): ${end - start}ms")
+    val topPosts = Array.tabulate(TopN)(i => posts(top5(i * 2 + 1)))
 
-    val relatedJson = upickle.default.write(allRelatedPosts)
-    os.write.over(os.pwd / os.up / "related_posts_scala.json", relatedJson)
-}
+    RelatedPost(post._id, post.tags, topPosts)
+  }
+
+  val end = System.currentTimeMillis()
+  println(s"Processing time (w/o IO): ${end - start}ms")
+
+  val relatedJson = upickle.default.write(allRelatedPosts)
+  os.write.over(os.pwd / os.up / "related_posts_scala.json", relatedJson)
