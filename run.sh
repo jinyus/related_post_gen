07072914eea0cb34f41ae9f8f7b678215ea76433
Nikita <vorontsov.nstu@yandex.ru>
2024-01-13 05:05:31 +0300

    fix time measurement

--- run.sh
+++ run.sh
@@ -547,7 +547,7 @@ run_erlang() {
 run_common_lisp_sbcl() {
     echo "Running Common Lisp (SBCL)" &&
         cd ./common-lisp &&
-        sbcl --load related.lisp
+        sbcl --non-interactive --load related.lisp
         run_command "Common Lisp (SBCL)" $slow_lang_runs ./related &&
         check_output "related-cl.json"
 }
