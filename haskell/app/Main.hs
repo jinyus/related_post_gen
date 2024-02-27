38b4a0f503582c7986a023a0aaca7325afa5a363
thelissimus <thelissimus@tuta.io>
2024-02-27 18:06:57 +0500

    refactor(haskell): qualify imports

--- haskell/app/Main.hs
+++ haskell/app/Main.hs
@@ -4,7 +4,6 @@
 {-# LANGUAGE DuplicateRecordFields #-}
 {-# LANGUAGE LambdaCase #-}
 {-# LANGUAGE OverloadedStrings #-}
-{-# LANGUAGE NoImplicitPrelude #-}
 
 module Main (main) where
 
@@ -19,22 +18,19 @@ import Data.Functor ((<&>))
 import Data.Kind (Type)
 import Data.Text (Text)
 import Data.Time.Clock.POSIX (getPOSIXTime)
-import Data.Vector (Vector, freeze, fromList, indexed, length, singleton, snoc, (!))
-import Data.Vector.Hashtables hiding (fromList)
-import Data.Vector.Mutable (STVector)
+import Data.Vector qualified as V
+import Data.Vector.Hashtables qualified as H
 import Data.Vector.Mutable qualified as VM
 
 import GHC.Generics (Generic)
 
-import Prelude hiding (length)
-
 type HashTable :: Type -> Type -> Type -> Type
-type HashTable s k v = Dictionary (PrimState (ST s)) VM.MVector k VM.MVector v
+type HashTable s k v = H.Dictionary (H.PrimState (ST s)) VM.MVector k VM.MVector v
 
 type Post :: Type
 data Post = MkPost
   { _id :: !Text
-  , tags :: !(Vector Text)
+  , tags :: !(V.Vector Text)
   , title :: !Text
   }
   deriving stock (Generic, Show)
@@ -43,8 +39,8 @@ data Post = MkPost
 type RelatedPosts :: Type
 data RelatedPosts = MkRelatedPosts
   { _id :: !Text
-  , tags :: !(Vector Text)
-  , related :: !(Vector Post)
+  , tags :: !(V.Vector Text)
+  , related :: !(V.Vector Post)
   }
   deriving stock (Generic, Show)
   deriving anyclass (FromJSON, ToJSON, NFData)
@@ -58,7 +54,7 @@ outputFile = "../related_posts_haskell.json"
 
 main :: IO ()
 main = do
-  Just (posts :: Vector Post) <- (evaluate . force) . decodeStrict =<< BS.readFile inputFile
+  Just (posts :: V.Vector Post) <- (evaluate . force) . decodeStrict =<< BS.readFile inputFile
 
   start <- getPOSIXTime
   relatedPosts <- evaluate . force $ runST (computeRelatedPosts posts)
@@ -67,42 +63,42 @@ main = do
   putStrLn $ "Processing time (w/o IO): " ++ show (round . (* 1000) $ end - start :: Int) ++ "ms"
   encodeFile outputFile relatedPosts
 
-computeRelatedPosts :: Vector Post -> ST s (Vector RelatedPosts)
+computeRelatedPosts :: V.Vector Post -> ST s (V.Vector RelatedPosts)
 computeRelatedPosts posts = do
-  tagMap <- initialize 0
+  tagMap <- H.initialize 0
 
-  let postsLen = length posts
-  let postsIdx = indexed posts
+  let postsLen = V.length posts
+  let postsIdx = V.indexed posts
 
   populateTagMap tagMap postsIdx
 
   forM postsIdx \(idx, MkPost{_id, tags}) -> do
-    (sharedTagsM :: STVector s Int) <- VM.replicate postsLen 0
+    (sharedTagsM :: VM.STVector s Int) <- VM.replicate postsLen 0
     countTags sharedTagsM tags tagMap
     VM.write sharedTagsM idx 0 -- exclude self from related posts
     topN <- calculateTopN sharedTagsM
-    let related = [1, 3 .. 10] <&> \n -> posts ! (topN ! n)
-    pure $ MkRelatedPosts{_id, tags, related = fromList related}
+    let related = [1, 3 .. 10] <&> \n -> posts V.! (topN V.! n)
+    pure $ MkRelatedPosts{_id, tags, related = V.fromList related}
 
-populateTagMap :: HashTable s Text (Vector Int) -> Vector (Int, Post) -> ST s ()
+populateTagMap :: HashTable s Text (V.Vector Int) -> V.Vector (Int, Post) -> ST s ()
 populateTagMap tagMap posts = do
   forM_ posts \(i, MkPost{tags}) ->
-    forM_ tags $ alterM tagMap (pure . Just . \case Just v -> snoc v i; Nothing -> singleton i)
+    forM_ tags $ H.alterM tagMap (pure . Just . \case Just v -> V.snoc v i; Nothing -> V.singleton i)
 
-countTags :: STVector s Int -> Vector Text -> HashTable s Text (Vector Int) -> ST s ()
+countTags :: VM.STVector s Int -> V.Vector Text -> HashTable s Text (V.Vector Int) -> ST s ()
 countTags sharedTagsM tags tagMap = do
   forM_ tags \tag -> do
-    idxs <- lookup' tagMap tag
+    idxs <- H.lookup' tagMap tag
     forM_ idxs $ VM.modify sharedTagsM (+ 1)
 
-calculateTopN :: STVector s Int -> ST s (Vector Int)
+calculateTopN :: VM.STVector s Int -> ST s (V.Vector Int)
 calculateTopN sharedTagsM = do
   let n = VM.length sharedTagsM
   topN <- VM.replicate (limitTopN * 2) 0
   loop sharedTagsM topN (0, n, 0)
-  freeze topN
+  V.freeze topN
  where
-  loop :: STVector s Int -> STVector s Int -> (Int, Int, Int) -> ST s ()
+  loop :: VM.STVector s Int -> VM.STVector s Int -> (Int, Int, Int) -> ST s ()
   loop sharedTags topN (i, len, minTags) = do
     when (i < len) do
       count <- VM.read sharedTags i
@@ -117,7 +113,7 @@ calculateTopN sharedTagsM = do
           loop sharedTags topN (i + 1, len, minTags')
         else loop sharedTags topN (i + 1, len, minTags)
 
-  calcUpperBound :: STVector s Int -> (Int, Int) -> ST s Int
+  calcUpperBound :: VM.STVector s Int -> (Int, Int) -> ST s Int
   calcUpperBound topN (upperBound, count) = do
     if upperBound >= 0
       then do
@@ -127,7 +123,7 @@ calculateTopN sharedTagsM = do
           else pure upperBound
       else pure upperBound
 
-vecmove :: STVector s Int -> Int -> STVector s Int -> Int -> Int -> ST s ()
+vecmove :: VM.STVector s Int -> Int -> VM.STVector s Int -> Int -> Int -> ST s ()
 vecmove src srcIdx dest destIdx len = do
   temp <- VM.clone (VM.slice srcIdx len src)
   VM.copy (VM.slice destIdx len dest) temp
